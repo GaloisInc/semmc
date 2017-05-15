1124317ff29535afa38679ae90e0b12e8a2e3258
Tristan Ravitch <tristan@nochair.net>
2017-05-14 22:55:09 -0700

    Start stubbing out the backend type

--- src/SemMC/Backend.hs
+++ src/SemMC/Backend.hs
@@ -1,3 +1,19 @@
+{-# LANGUAGE ExistentialQuantification #-}
 -- | The definition of the backend implementation that must be provided for each
 -- architecture
-module SemMC.Backend () where
+module SemMC.Backend (
+  Backend(..)
+  ) where
+
+import qualified Data.ByteString as BS
+import qualified System.Random.MWC as R
+
+data Backend opcode operand =
+  forall a .
+  Backend { mkTestCase :: R.GenIO -> IO a
+          , testCaseCost :: a -> a -> Double
+          , serializeTestCase :: a -> BS.ByteString
+          -- ^ Serialize for wire transmission
+          , deserializeTestCase :: BS.ByteString -> Maybe a
+          -- ^ Read a result state back
+          }
