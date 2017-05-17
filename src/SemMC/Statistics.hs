2e9885228a9d75ea30100e1ecd4311a22993ab18
Tristan Ravitch <tristan@nochair.net>
2017-05-16 22:29:17 -0700

    Hide more logging details
    Make the logging shutdown synchronous

--- src/SemMC/Statistics.hs
+++ src/SemMC/Statistics.hs
@@ -2,37 +2,43 @@
 module SemMC.Statistics (
   StatisticsThread,
   createStatisticsThread,
-  StatisticsRecord(..),
-  logStatistics
+  terminate
   ) where
 
 import qualified Control.Concurrent as C
 import qualified Control.Concurrent.STM as STM
 
 data StatisticsThread =
-  StatisticsThread { stChan :: STM.TChan StatisticsRecord
+  StatisticsThread { stMsgChan :: STM.TChan StatisticsRecord
+                   , stTermChan :: STM.TChan ()
                    , stThread :: C.ThreadId
                    }
 
 createStatisticsThread :: IO StatisticsThread
 createStatisticsThread = do
-  chan <- STM.newTChanIO
-  tid <- C.forkIO (statisticsLoop chan)
-  return StatisticsThread { stChan = chan
+  msgChan <- STM.newTChanIO
+  termChan <- STM.newTChanIO
+  tid <- C.forkIO (statisticsLoop termChan msgChan)
+  return StatisticsThread { stMsgChan = msgChan
+                          , stTermChan = termChan
                           , stThread = tid
                           }
 
-statisticsLoop :: STM.TChan StatisticsRecord -> IO ()
-statisticsLoop chan = do
-  msg <- STM.atomically $ STM.readTChan chan
+statisticsLoop :: STM.TChan () -> STM.TChan StatisticsRecord -> IO ()
+statisticsLoop termChan msgChan = do
+  msg <- STM.atomically $ STM.readTChan msgChan
 
   case msg of
-    Terminate -> return ()
-    _ -> statisticsLoop chan
+    Terminate -> STM.atomically $ STM.writeTChan termChan ()
+
+    _ -> statisticsLoop termChan msgChan
 
 data StatisticsRecord = Terminate
                       | SolverInvocation Int
   deriving (Eq, Ord, Show)
 
-logStatistics :: StatisticsThread -> StatisticsRecord -> IO ()
-logStatistics t r = STM.atomically (STM.writeTChan (stChan t) r)
+terminate :: StatisticsThread -> IO ()
+terminate st = do
+  STM.atomically $ STM.writeTChan (stMsgChan st) Terminate
+  () <- STM.atomically $ STM.readTChan (stTermChan st)
+  return ()
