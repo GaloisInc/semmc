f63cb52cf3b9783bc2377a10a669469ace2f37f9
John Grosen <jgrosen@galois.com>
2017-06-27 18:42:33 -0700

    Better document SemMC.Synthesis.Template

--- src/SemMC/Synthesis/Template.hs
+++ src/SemMC/Synthesis/Template.hs
@@ -1,3 +1,12 @@
+{-|
+Module  : SemMC.Synthesis.Template
+
+Provides a method of working with "templated" instructions. These are
+instructions that have concrete register operands, but abstract immediate
+operands. This allows an SMT solver to later "fill in" the abstract operands,
+given constraints.
+
+-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE FlexibleContexts #-}
@@ -52,8 +61,11 @@ import           SemMC.Formula
 import           SemMC.Formula.Instantiate
 import           SemMC.Util
 
+-- | Type used in an 'OperandList' for a templated instruction.
 data TemplatedOperand (arch :: *) (s :: Symbol) where
+  -- | A filled-in operand -- in practice, a particular register.
   Concrete :: Operand arch s -> TemplatedOperand arch s
+  -- | A blank operand -- an immediate.
   Abstract :: (KnownSymbol s) => BaseTypeRepr (OperandType arch s) -> TemplatedOperand arch s
 
 instance (ShowF (Operand arch)) => ShowF (TemplatedOperand arch) where
@@ -68,6 +80,8 @@ instance (TestEquality (Operand arch)) => TestEquality (TemplatedOperand arch) w
 
 instance (IsOperand (Operand arch)) => IsOperand (TemplatedOperand arch)
 
+-- | Phantom architecture used to signal we want to use template operands when
+-- instantiating a formula, rather than all concrete operands.
 data TemplatedArch (arch :: *)
 
 type instance Operand (TemplatedArch arch) = TemplatedOperand arch
@@ -89,17 +103,8 @@ instance (TemplateConstraints arch) => Architecture (TemplatedArch arch) where
   operandToLocation _ (Concrete op) = operandToLocation (Proxy :: Proxy arch) op
   operandToLocation _ (Abstract _)  = Nothing
 
-  -- valueToOperand?
-
-data TemplatedFormula sym arch sh =
-  TemplatedFormula { tfOperandList :: OperandList (TemplatedOperand arch) sh
-                   , tfOperandExprs :: OperandList (WrappedExpr sym (TemplatedArch arch)) sh
-                   , tfFormula :: Formula sym (TemplatedArch arch)
-                   }
-deriving instance (ShowF (Operand arch), ShowF (TemplatedOperand arch), ShowF (S.SymExpr sym), ShowF (S.BoundVar sym), ShowF (Location arch)) => Show (TemplatedFormula sym arch sh)
-
-instance (ShowF (Operand arch), ShowF (TemplatedOperand arch), ShowF (S.SymExpr sym), ShowF (S.BoundVar sym), ShowF (Location arch)) => ShowF (TemplatedFormula sym arch) where
-  showF = show
+  -- ?
+  valueToOperand = undefined
 
 -- | Convert a 'ParameterizedFormula' that was created using a 'TemplatedArch'
 -- to the base architecture, using 'unsafeCoerce'.
@@ -148,6 +153,19 @@ unTemplate :: (OrdF (Location arch))
            -> ParameterizedFormula sym arch sh
 unTemplate = unTemplateUnsafe
 
+
+-- | 'Formula' along with the expressions that correspond to each operand (for
+-- pulling out the values of immediates after solving, primarily).
+data TemplatedFormula sym arch sh =
+  TemplatedFormula { tfOperandList :: OperandList (TemplatedOperand arch) sh
+                   , tfOperandExprs :: OperandList (WrappedExpr sym (TemplatedArch arch)) sh
+                   , tfFormula :: Formula sym (TemplatedArch arch)
+                   }
+deriving instance (ShowF (Operand arch), ShowF (TemplatedOperand arch), ShowF (S.SymExpr sym), ShowF (S.BoundVar sym), ShowF (Location arch)) => Show (TemplatedFormula sym arch sh)
+
+instance (ShowF (Operand arch), ShowF (TemplatedOperand arch), ShowF (S.SymExpr sym), ShowF (S.BoundVar sym), ShowF (Location arch)) => ShowF (TemplatedFormula sym arch) where
+  showF = show
+
 class KnownBool (b :: Bool) where
   boolVal :: Proxy b -> Bool
 
@@ -157,8 +175,11 @@ instance KnownBool 'False where
 instance KnownBool 'True where
   boolVal _ = True
 
+-- | The only way to define structure-dependent operations on type-level lists...
 class TemplatableOperands arch sh where
+  -- | For a given shape, generate all possible templated operand lists.
   makeTemplatedOpLists :: [OperandList (TemplatedOperand arch) sh]
+  -- | Recover the resulting concrete operands once the SMT solver has run.
   recoverOperands :: GroundEvalFn t
                   -> OperandList (TemplatedOperand arch) sh
                   -> OperandList (WrappedExpr (S.SimpleBackend t) (TemplatedArch arch)) sh
@@ -191,12 +212,15 @@ instance (Architecture arch,
     (:>) <$> (valueToOperand (Proxy :: Proxy arch) <$> groundEval evalFn expr)
          <*> recoverOperands evalFn restOps restExprs
 
+-- | Make a list of all possible 'TemplatedFormula's from a given
+-- 'ParameterizedFormula'.
 templatizeFormula :: (TemplateConstraints arch,
                       TemplatableOperands arch sh)
                   => S.SimpleBuilder t st
                   -> ParameterizedFormula (S.SimpleBuilder t st) (TemplatedArch arch) sh
                   -> [IO (TemplatedFormula (S.SimpleBuilder t st) arch sh)]
-templatizeFormula sym pf = map (\ol -> uncurry (TemplatedFormula ol) <$> instantiateFormula sym pf ol) makeTemplatedOpLists
+templatizeFormula sym pf = map mkFormula makeTemplatedOpLists
+  where mkFormula ol = uncurry (TemplatedFormula ol) <$> instantiateFormula sym pf ol
 
 templatizeFormula' :: (TemplateConstraints arch,
                        TemplatableOperands arch sh)
@@ -207,6 +231,8 @@ templatizeFormula' sym = sequence . templatizeFormula sym
 
 type TemplatableOpcode arch = Witness (TemplatableOperands arch) ((Opcode arch) (Operand arch))
 
+-- | An opcode along with a 'TemplatedFormula' that implements it for specific
+-- templated operands.
 data TemplatedInstructionFormula sym arch where
   TemplatedInstructionFormula :: (TemplatableOperands arch sh)
                               => (Opcode arch) (Operand arch) sh
@@ -226,6 +252,7 @@ foo xs = join [foo' i | i <- [1..]]
         foo' 1 = map pure xs
         foo' n = [x : xs' | x <- xs, xs' <- foo' (n-1)]
 
+-- | An infinite stream of templated instruction sequences.
 templatedInstructions :: (TemplateConstraints arch)
                       => S.SimpleBuilder t st
                       -> MapF.MapF (TemplatableOpcode arch) (ParameterizedFormula (S.SimpleBuilder t st) (TemplatedArch arch))
