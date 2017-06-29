02c40c089d530047c7b073dc58d6350638dd5eaf
Tristan Ravitch <tristan@nochair.net>
2017-06-29 09:40:51 -0700

    remote-runner: cleanup and documentation

--- tools/remote-runner.c
+++ tools/remote-runner.c
@@ -11,15 +11,33 @@
 #include <ucontext.h>
 
 /*
+  This program is designed to be a remote runner of programs with test vectors.  It:
 
-  TODO:
+  1) reads a test vector and test program off of stdin,
+  2) maps the test program as executable (splicing in code at the end to raise a SIGTRAP),
+  3) catches the SIGTRAP and takes a snapshot of the register and memory state, and
+  4) sends the result out over stdout.
 
-  - Deal with endianness.  This is kind of problematic if we need to byte swap
-    values in mcontext_t
 
-  - Investigate alignment requirements for the stack and memory regions
+  Notes:
 
-  - Should we include the stack data as part of the payload?  Probably not...
+  * We don't deal with endianness; it is the responsibility of the test
+    generator to send data in the correct endianness
+
+  * The state of the program stack isn't tracked for test programs
+
+  * The value of the IP after a test program isn't particularly meaningful due
+    to the extra code to raise a SIGTRAP, and should be used carefully (if at all)
+
+  * Most run-time errors are handled and reported over the channel.  A few things
+    are handled via assertions.
+
+  * Test cases are each labeled with a nonce that is written back onto the channel
+
+  * Tests are run in-order and responses are delivered in-order.  Nonces are
+    used anyway for clarity.  Responses to parse failures do not have a nonce.
+    That probably isn't a big deal, though, because those failures can only
+    occur if the stream ends early.
 
  */
 
@@ -29,6 +47,7 @@
 typedef enum {
   WORK_ITEM = 0,
   WORK_DONE = 1,
+  // Possible parse errors
   WORK_ERROR_NONONCE = 2,
   WORK_ERROR_NOCONTEXT = 3,
   WORK_ERROR_NOMEM = 4,
@@ -39,9 +58,11 @@ typedef enum {
 
 typedef enum {
   RESPONSE_SUCCESS = 0,
+  // Error reading a WorkItem (followed by a WorkTag as a uint16_t)
   RESPONSE_READ_ERROR = 1,
+  // Received a SIGILL or SIGSEGV (followed by an int32_t with the signal number)
   RESPONSE_SIGNAL_ERROR = 2,
-  // Failed to map the program into memory
+  // Failed to map the program into memory (no metadata)
   RESPONSE_ERROR_MAPFAILED = 7
 } ResponseTag;
 
@@ -52,8 +73,7 @@ typedef enum {
   SIGNAL_OTHER = 1
 } SignalTag;
 
-// The definition of a work item; note that these must be heap allocated, as they
-// will be freed
+// The definition of a work item
 typedef struct {
   // The unique identifier of this work item.  It only needs to be unique in this process.
   uint64_t nonce;
@@ -78,7 +98,8 @@ typedef struct {
   // The number of bytes occupied by the test program
   uint16_t programByteCount;
 
-  // The actual bytes of the program to be executed
+  // The actual bytes of the program to be executed; note that this is
+  // dynamically allocated, and must be freed with `freeWorkItem`.
   uint8_t* program;
 } WorkItem;
 
@@ -137,18 +158,10 @@ void applyMContextMask(mcontext_t* mctx, mcontext_t* mctxMask, uint8_t* mem1, ui
 
 #endif
 
-// Release the dynamic (program) bytes occupied by a work item
-void freeWorkItem(WorkItem* item) {
-  free(item->program);
-  item->program = NULL;
-}
-
-/*
-  Allocate space for the program in the work item based on the
-  `programByteCount` field.
-
-  If the byte count is zero, this function is a no-op.
- */
+// Allocate space for the program in the work item based on the
+// `programByteCount` field.
+//
+// If the byte count is zero, this function is a no-op.
 void allocateProgramSpace(WorkItem* item) {
   if(item->programByteCount == 0)
     return;
@@ -158,7 +171,6 @@ void allocateProgramSpace(WorkItem* item) {
 
 
 /*
-
   Read a work item off of the stream (or report an error/end of work).
 
   The first thing we do is check the type of work coming at us (either a marker
@@ -173,8 +185,8 @@ void allocateProgramSpace(WorkItem* item) {
   4) Another mcontext_t that acts as a mask
   5) A 32 byte buffer representing the state of the first distinguished memory location
   6) A 32 byte buffer representing the state of the second distinguished memory location
-  7) The program to run, prefixed with a uint16_t indicating the number of bytes to read
-
+  7) A uint16_t denoting the number of bytes in the test program
+  8) The program to run
  */
 WorkTag readWorkItem(FILE* stream, WorkItem* item) {
   uint8_t tagByte;
@@ -229,19 +241,28 @@ WorkTag readWorkItem(FILE* stream, WorkItem* item) {
   return WORK_ITEM;
 }
 
+// Allocate a full page (aligned to a page boundary).
+//
+// mmap guarantees that its result will be aligned to a page boundary, so just
+// use that for simplicity.
+void* allocatePage() {
+  void* res = mmap(NULL, pageSize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  // mmap doesn't return NULL on failure, because some systems can map the zero
+  // page.  We don't have to worry about that on modern Linux.
+  if(res == MAP_FAILED)
+    return NULL;
+
+  return res;
+}
+
 typedef void (*Program)(void);
 
 Program mapProgram(WorkItem* item) {
   int totalBytes = item->programByteCount + sizeof(raiseTrap);
   assert(totalBytes < pageSize);
 
-  /*
-    Allocate a page (we need it to be page aligned for mprotect, and mmap
-    guarantees that its return is page aligned).
-   */
-  uint8_t* program = mmap(NULL, pageSize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-  if(program == MAP_FAILED)
-    return NULL;
+  uint8_t* program = allocatePage();
+  if(!program) return NULL;
 
   memcpy(program, item->program, item->programByteCount);
   memcpy(program + item->programByteCount, raiseTrap, sizeof(raiseTrap));
@@ -254,7 +275,6 @@ Program mapProgram(WorkItem* item) {
   return NULL;
 }
 
-
 // Apply any fixes to the context required to make it valid
 void fixupContext(ucontext_t* ctx, WorkItem* item) {
   ctx->uc_mcontext = item->ctx;
@@ -264,32 +284,15 @@ void fixupContext(ucontext_t* ctx, WorkItem* item) {
   applyMContextMask(&ctx->uc_mcontext, &item->ctxMask, item->mem1, item->mem2);
   // When we return from executing the program, we are in theory going to return
   // to restoreCtx (the main thread of execution).  That said, this link might
-  // never need to really be followed because we never really get to the end of
-  // that context (i.e., we always throw a synchronous signal instead of
-  // returning).  It is set for good form...
+  // never need to be followed because we never really get to the end of that
+  // context (i.e., we always throw a synchronous signal instead of returning,
+  // and we jump back to restoreCtx from that signal handler).  It is set for
+  // good form...
   ctx->uc_link = &restoreCtx;
 }
 
-/*
-  Write the results of a test back into the stream.
-
-  This is in the same order as the input test vector; it can elide the context
-  mask, since we'll know what that was already on the server side due to the
-  nonce.  Likewise, we don't need to include the program.
-
-  Note that the response needs a tag, but that is written by `writeWorkResponse`
- */
-void writeTestResult(FILE* stream, WorkItem* item, ucontext_t* ctx) {
-  fwrite(&item->nonce, sizeof(item->nonce), 1, stream);
-  fwrite(&ctx, sizeof(ctx), 1, stream);
-  fwrite(item->mem1, sizeof(item->mem1), 1, stream);
-  fwrite(item->mem2, sizeof(item->mem2), 1, stream);
-}
-
-/*
-  Take a work item, map the program (possibly with modifications), then jump to
-  it.
- */
+// Take a work item, map the program (possibly with modifications), then jump to
+// it.
 ResponseTag processWorkItem(WorkItem* item) {
   Program p = mapProgram(item);
   if(!p) return RESPONSE_ERROR_MAPFAILED;
@@ -306,6 +309,10 @@ ResponseTag processWorkItem(WorkItem* item) {
     return RESPONSE_SIGNAL_ERROR;
 }
 
+// A handler for traps that captures the current CPU state into `signalCtx` and
+// then tells the handler to return to `restoreCtx`.  We mark the handling as
+// "successful" to mean that the test program completed without raising a
+// segfault or executing an illegal instruction.
 void trapHandler(int sigNum, siginfo_t* info, void* ctxp) {
   ucontext_t* ctx = ctxp;
   signalCtx = *ctx;
@@ -326,14 +333,12 @@ void otherSigHandler(int sigNum) {
   otherSignal = sigNum;
 }
 
-/*
-  Write a message indicating a read error to the channel.
-
-  1) ResponseTag (RESPONSE_READ_ERROR)
-  2) WorkTag (indicating nature of failure)
-
-  Both are uint16_t
- */
+// Write a message indicating a read error to the channel.
+//
+// 1) ResponseTag (RESPONSE_READ_ERROR)
+// 2) WorkTag (indicating nature of failure)
+//
+// Both are uint16_t
 void writeReadErrorResult(FILE* stream, WorkTag wtag, const char* msg) {
   fprintf(stderr, "%s\n", msg);
   uint16_t tagBytes = RESPONSE_READ_ERROR;
@@ -342,10 +347,14 @@ void writeReadErrorResult(FILE* stream, WorkTag wtag, const char* msg) {
   fwrite(&tagBytes, sizeof(tagBytes), 1, stream);
 }
 
+// Write a response back onto the stream.  It will either be an error with
+// metadata or a success with a final processor state.
 void writeWorkResponse(FILE* stream, ResponseTag rtag, WorkItem* item) {
-  // All responses start off with a uint16_t ResponseTag code
+  // All responses start off with a uint16_t ResponseTag code followed by a
+  // nonce
   uint16_t tagBytes = rtag;
   fwrite(&tagBytes, sizeof(tagBytes), 1, stream);
+  fwrite(&item->nonce, sizeof(item->nonce), 1, stream);
 
   switch(rtag) {
   case RESPONSE_READ_ERROR:
@@ -356,7 +365,9 @@ void writeWorkResponse(FILE* stream, ResponseTag rtag, WorkItem* item) {
     fwrite(&otherSignal, sizeof(otherSignal), 1, stream);
     break;
   case RESPONSE_SUCCESS:
-    writeTestResult(stream, item, &signalCtx);
+    fwrite(&signalCtx.uc_mcontext, sizeof(signalCtx.uc_mcontext), 1, stream);
+    fwrite(item->mem1, sizeof(item->mem1), 1, stream);
+    fwrite(item->mem2, sizeof(item->mem2), 1, stream);
     break;
   }
 }
@@ -372,8 +383,6 @@ void writeWorkResponse(FILE* stream, ResponseTag rtag, WorkItem* item) {
 // We are also on the lookout for SIGSEGV in case we accessed memory we
 // shouldn't.
 void setupSignalHandlers() {
-  // Set up a signal handler for SIGTRAP.  This is the magic handler that lets
-  // us save contexts after execution.
   sigset_t mask;
   sigemptyset(&mask);
   struct sigaction sa;
@@ -392,10 +401,13 @@ int main(int argc, char* argv[]) {
   pageSize = sysconf(_SC_PAGESIZE);
   // This is the stack that will be used for executing the programs we get in
   // work items.  Using mmap to ensure that it is 4k aligned, though that
-  // probably isn't really necessary.
-  programStack = mmap(NULL, pageSize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  // probably isn't really necessary.  The one nice thing about using mmap
+  // instead of allocating the new stack on the real stack is that overflows
+  // (highly unlikely) will hit an unmapped page instead of spilling onto the
+  // main stack.
+  programStack = allocatePage();
+  assert(programStack);
   setupSignalHandlers();
-  assert(programStack != MAP_FAILED);
 
   do {
     WorkItem item;
@@ -425,9 +437,7 @@ int main(int argc, char* argv[]) {
     case WORK_ITEM:
       rtag = processWorkItem(&item);
       writeWorkResponse(stdout, rtag, &item);
-      // Note that this doesn't free the item itself, which is stored on the
-      // stack.  It only stores the heap-allocated program portion.
-      freeWorkItem(&item);
+      free(item.program);
       break;
     }
   } while(1);
