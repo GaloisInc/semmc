fc2a4e50c6e0b30b91e2e7b7e4dd80576d7aa0ea
Tristan Ravitch <tristan@nochair.net>
2017-07-08 10:35:07 -0700

    Implement formula loading in the core stochastic learning harness

--- semmc/src/SemMC/Strata.hs
+++ semmc/src/SemMC/Strata.hs
@@ -1,3 +1,8 @@
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module SemMC.Strata (
   Config(..),
   strata
@@ -5,18 +10,30 @@ module SemMC.Strata (
 
 import qualified Control.Concurrent as C
 import Control.Monad ( replicateM )
+import qualified Control.Monad.Catch as E
+import Data.Maybe ( catMaybes )
 import Data.Monoid
 import qualified Data.Set as S
+import qualified Data.Traversable as T
+import System.FilePath ( (</>), (<.>) )
 
 import Prelude
+
+import qualified Data.Parameterized.Some as P
+import qualified Data.Parameterized.Classes as P
+
+import qualified Lang.Crucible.Solver.Interface as CRU
 
 import qualified Dismantle.Arbitrary as A
 import qualified Dismantle.Instruction as D
 import qualified Dismantle.Instruction.Random as D
 
-import SemMC.Backend ( Backend(..) )
+import SemMC.Architecture ( Architecture, Opcode, Operand )
 import qualified SemMC.Statistics as Stats
 import qualified SemMC.Worklist as WL
+import qualified SemMC.Formula as F
+import qualified SemMC.Formula.Parser as FP
+import SemMC.Util ( Witness(..) )
 
 -- Have this process the worklist in a single-threaded way, but for each work
 -- item spawn off an Async thread to process it.  Wait once there are enough
@@ -27,44 +44,78 @@ data Config = Config { numThreads :: Int
                      , learnedSetDir :: FilePath
                      }
 
-strata :: (D.ArbitraryOperands opcode operand) => Config -> Backend opcode operand -> IO [(instr, semantics)]
-strata cfg backend = do
+strata :: (CRU.IsExprBuilder sym, CRU.IsSymInterface sym, Architecture arch, D.ArbitraryOperands (Opcode arch) (Operand arch))
+       => Config
+       -> sym
+       -> proxy arch
+       -> [P.Some (Witness (FP.BuildOperandList arch) ((Opcode arch) (Operand arch)))]
+       -> IO [(instr, semantics)]
+strata cfg sym backend allOpcodes = do
   stats <- Stats.createStatisticsThread
-  baseSet <- loadFormulas (baseSetDir cfg)
-  learnedSet <- loadFormulas (learnedSetDir cfg)
+  baseSet <- loadFormulas sym allOpcodes (baseSetDir cfg)
+  learnedSet <- loadFormulas sym allOpcodes (learnedSetDir cfg)
   let initialFormulas = baseSet <> learnedSet
-  worklist <- buildWorklist (allOpcodes backend) initialFormulas
+  worklist <- buildWorklist backend (map dropWitness allOpcodes) initialFormulas
   threads <- replicateM (numThreads cfg) (C.forkIO (processWorklist stats backend worklist))
   Stats.terminate stats
   return undefined
 
-processWorklist :: (D.ArbitraryOperands opcode operand)
+dropWitness :: P.Some (Witness c f) -> P.Some f
+dropWitness (P.Some (Witness a)) = P.Some a
+
+processWorklist :: (Architecture arch, D.ArbitraryOperands (Opcode arch) (Operand arch))
                 => Stats.StatisticsThread
-                -> Backend opcode operand
-                -> WL.Worklist (D.SomeOpcode opcode operand)
+                -> proxy arch -- Backend opcode operand
+                -> WL.Worklist (P.Some (Opcode arch (Operand arch)))
                 -> IO ()
 processWorklist _stats _backend wl = do
   mwork <- WL.takeWork wl
   case mwork of
     Nothing -> return ()
-    Just someOp -> do
+    Just (P.Some someOp) -> do
       -- FIXME: Put this into a state
       gen <- A.createGen
-      Just target <- D.randomInstruction gen (S.singleton someOp)
+      Just target <- D.randomInstruction gen (S.singleton (D.SomeOpcode someOp))
       return ()
 
-data FormulaSet = FormulaSet
+data FormulaSet sym arch = FormulaSet [(P.Some (F.ParameterizedFormula sym arch))]
 
-instance Monoid FormulaSet where
-  mempty = FormulaSet
+instance Monoid (FormulaSet sym arch) where
+  mempty = FormulaSet []
   mappend = undefined
 
-loadFormulas :: FilePath -> IO FormulaSet
-loadFormulas = undefined
+data StratifyError = FormulaParseError String
+  deriving (Show)
 
-buildWorklist :: [D.SomeOpcode opcode operand] -> FormulaSet -> IO (WL.Worklist (D.SomeOpcode opcode operand))
-buildWorklist allOps knownFormulas = do
-  let workitems :: [D.SomeOpcode opcode operand]
+instance E.Exception StratifyError
+
+loadFormulas :: forall sym arch
+              . (CRU.IsExprBuilder sym, CRU.IsSymInterface sym, Architecture arch)
+             => sym
+             -> [P.Some (Witness (FP.BuildOperandList arch) ((Opcode arch) (Operand arch)))]
+             -> FilePath
+             -> IO (FormulaSet sym arch)
+loadFormulas sym allOpcodes formulaDir = do
+  mFormulas <- T.forM allOpcodes $ \(P.Some (Witness oc)) -> fmap P.Some <$> readFormulaForOpcode oc
+  return (FormulaSet (catMaybes mFormulas))
+  where
+    readFormulaForOpcode :: (FP.BuildOperandList arch sh)
+                         => (Opcode arch) (Operand arch) sh
+                         -> IO (Maybe (F.ParameterizedFormula sym arch sh))
+    readFormulaForOpcode oc = do
+      let fp = formulaDir </> P.showF oc <.> "sem"
+      ef <- FP.readFormulaFromFile sym fp
+      case ef of
+        Left _ -> return Nothing
+        Right f -> return (Just f)
+
+buildWorklist :: forall arch sym proxy
+               . proxy arch
+              -> [P.Some (Opcode arch (Operand arch))]
+              -> FormulaSet sym arch
+              -> IO (WL.Worklist (P.Some (Opcode arch (Operand arch))))
+buildWorklist _proxy allOps knownFormulas = do
+  let workitems :: [P.Some (Opcode arch (Operand arch))]
       workitems = undefined allOps knownFormulas
   WL.newWorklist workitems
 
