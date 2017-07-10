47342c31a3a2ffd478bcc0c79d336eaf0cc97a9d
Tristan Ravitch <tristan@nochair.net>
2017-07-10 16:14:18 -0700

    Shuffle the types for EquivalenceClasses
    It isn't used yet, but EquivalenceClass will be used later

--- semmc/src/SemMC/Stochastic/Classify.hs
+++ semmc/src/SemMC/Stochastic/Classify.hs
@@ -12,15 +12,15 @@ import qualified Data.Set as S
 import SemMC.Architecture ( Instruction )
 import SemMC.Stochastic.Monad
 
-data EquivalenceClasses arch = EquivalenceClasses { unClasses :: S.Set (EquivalenceClass arch) }
+data EquivalenceClasses arch = EquivalenceClasses { unClasses :: S.Set (S.Set [Instruction arch]) }
 
 data EquivalenceClass arch = EquivalenceClass { unClass :: S.Set [Instruction arch] }
 
 equivalenceClasses :: [Instruction arch] -> EquivalenceClasses arch
-equivalenceClasses p = EquivalenceClasses (S.singleton (EquivalenceClass (S.singleton p)))
+equivalenceClasses p = EquivalenceClasses (S.singleton (S.singleton p))
 
 countPrograms :: EquivalenceClasses arch -> Int
-countPrograms s = sum (map (S.size . unClass) (S.toList (unClasses s)))
+countPrograms s = sum (map S.size (S.toList (unClasses s)))
 
 classify :: [Instruction arch] -> EquivalenceClasses arch -> Syn sym arch (Maybe (EquivalenceClasses arch))
 classify = undefined
