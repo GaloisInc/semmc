e16648fe9634817b299b39d1f34d13a812795a71
Tristan Ravitch <tristan@nochair.net>
2017-07-11 18:14:37 -0700

    Start threading through some state for input/output relations

--- semmc/src/SemMC/Stochastic/Strata.hs
+++ semmc/src/SemMC/Stochastic/Strata.hs
@@ -11,13 +11,12 @@ module SemMC.Stochastic.Strata (
 import Control.Monad.Trans ( liftIO )
 import qualified Data.Set.NonEmpty as NES
 
-import Data.Parameterized.Classes ( OrdF )
 import qualified Data.Parameterized.Map as MapF
 import Data.Parameterized.Some ( Some(..) )
 
 import qualified Dismantle.Instruction.Random as D
 
-import SemMC.Architecture ( Architecture, Instruction, Opcode, Operand )
+import SemMC.Architecture ( Architecture, Instruction, Opcode, Operand, Location )
 import qualified SemMC.Formula as F
 
 import qualified SemMC.Stochastic.Classify as C
@@ -60,7 +59,7 @@ strataOne :: (Architecture arch, D.ArbitraryOperands (Opcode arch) (Operand arch
           => Opcode arch (Operand arch) sh
           -> Syn t arch (Maybe (F.ParameterizedFormula (Sym t) arch sh))
 strataOne op = do
-  instr <- instantiateInstruction op
+  instr <- instantiateInstruction op undefined -- Add in the input relation
   mprog <- synthesize instr
   case mprog of
     Nothing -> return Nothing
@@ -94,23 +93,36 @@ finishStrataOne :: Opcode arch (Operand arch) sh
 finishStrataOne op instr eqclasses = do
   bestClass <- C.chooseClass eqclasses
   prog <- C.chooseProgram bestClass
-  buildFormula op instr prog
+  buildFormula op instr undefined prog
 
 -- | Construct a formula for the given instruction based on the selected representative program.
 --
 -- We pass in the opcode because we need the shape of the opcode in the type signature.
+--
+-- Here we need the set of output locations (which could include implicit locations)
 buildFormula :: Opcode arch (Operand arch) sh
              -> Instruction arch
+             -> [Some (Location arch)]
+             -- ^ Output locations for the opcode
              -> [Instruction arch]
              -> Syn t arch (F.ParameterizedFormula (Sym t) arch sh)
 buildFormula = undefined
 
-instantiateInstruction :: (D.ArbitraryOperands (Opcode arch) (Operand arch), OrdF (Opcode arch (Operand arch)))
+-- | Generate an arbitrary instruction for the given opcode.
+--
+-- Note that this function should strive to avoid operands that are distinct
+-- from any implicit operands.  It may also want to ensure that registers are
+-- not reused.  To do that, we'll need to pass in the set of input locations
+--
+-- We'll also want to return a mapping from locations to input values to be used
+-- to inform the initial state generation.
+instantiateInstruction :: (Architecture arch, D.ArbitraryOperands (Opcode arch) (Operand arch))
                        => Opcode arch (Operand arch) sh
+                       -> [Some (Location arch)]
+                       -- ^ Input locations for the opcode
                        -> Syn t arch (Instruction arch)
-instantiateInstruction op = do
+instantiateInstruction op _inputs = do
   gen <- askGen
-  -- Note: randomInstruction can only return Nothing if the set it is given is
-  -- empty.  We should probably change it to accept a non-empty list.
   target <- liftIO $ D.randomInstruction gen (NES.singleton (Some op))
   return target
+
