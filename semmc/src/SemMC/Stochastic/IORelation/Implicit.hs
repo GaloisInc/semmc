2bb2f3c43ef0592018e8807fce87508ebe4727aa
Tristan Ravitch <tristan@nochair.net>
2017-07-20 16:57:25 -0700

    Fill out more of the implicit operand discovery code
    Just have to deal with test case generation now (and implicit input operands)

--- semmc/src/SemMC/Stochastic/IORelation/Implicit.hs
+++ semmc/src/SemMC/Stochastic/IORelation/Implicit.hs
@@ -6,14 +6,22 @@ module SemMC.Stochastic.IORelation.Implicit (
   findImplicitOperands
   ) where
 
+import qualified GHC.Err.Located as L
+
 import Control.Monad ( replicateM )
 import qualified Control.Monad.State.Strict as St
 import Control.Monad.Trans ( liftIO )
 import qualified Data.Foldable as F
+import qualified Data.Map.Strict as M
+import Data.Monoid
+import qualified Data.Set as S
 
 import qualified Data.Set.NonEmpty as NES
+import qualified Data.Parameterized.Classes as P
+import qualified Data.Parameterized.Map as MapF
 import Data.Parameterized.Some ( Some(..) )
 
+import qualified Dismantle.Instruction as D
 import qualified Dismantle.Instruction.Random as D
 
 import SemMC.Architecture
@@ -60,9 +68,50 @@ computeImplicitOperands :: (Architecture arch)
                         -> [TestBundle (R.TestCase (ArchState (Sym t) arch)) (ImplicitFact arch)]
                         -> [R.ResultOrError (ArchState (Sym t) arch)]
                         -> M t arch (IORelation arch sh)
-computeImplicitOperands op tests results = undefined idx
+computeImplicitOperands op tests results =
+  F.foldlM (buildImplicitRelation op idx) mempty tests
   where
     idx = F.foldl' indexResults emptyResultIndex results
+
+-- |
+--
+-- Note that the operand isn't used - it is acting like a proxy for the @sh@ parameter.
+buildImplicitRelation :: (Architecture arch)
+                      => Opcode arch (Operand arch) sh
+                      -> ResultIndex (ArchState (Sym t) arch)
+                      -> IORelation arch sh
+                      -> TestBundle (R.TestCase (ArchState (Sym t) arch)) (ImplicitFact arch)
+                      -> M t arch (IORelation arch sh)
+buildImplicitRelation _op rix iorel tb = do
+  implicitOutputLocs <- S.unions <$> mapM (collectImplicitOutputLocations rix (tbResult tb)) (tbTestCases tb)
+  let newRel = IORelation { inputs = []
+                          , outputs = map ImplicitOperand (F.toList implicitOutputLocs)
+                          }
+  return (iorel <> newRel)
+
+collectImplicitOutputLocations :: (Architecture arch)
+                               => ResultIndex (ArchState (Sym t) arch)
+                               -> ImplicitFact arch
+                               -> R.TestCase (ArchState (Sym t) arch)
+                               -> M t arch (S.Set (Some (Location arch)))
+collectImplicitOutputLocations rix f tc =
+  case M.lookup (R.testNonce tc) (riSuccesses rix) of
+    Nothing -> return S.empty
+    Just res ->
+      case f of
+        ImplicitFact { ifExplicits = explicitOperands } ->
+          F.foldrM (addLocIfImplicitAndDifferent (S.fromList explicitOperands)) S.empty (MapF.toList (R.resultContext res))
+  where
+    addLocIfImplicitAndDifferent explicitOperands pair s =
+      case pair of
+        MapF.Pair loc postVal
+          | Some loc `S.member` explicitOperands -> return s
+          | otherwise ->
+            case MapF.lookup loc (R.testContext tc) of
+              Nothing -> L.error ("Expected location in state: " ++ P.showF loc)
+              Just preVal
+                | preVal == postVal -> return s
+                | otherwise -> return (S.insert (Some loc) s)
 
 -- | For an instruction instance, sweep across the parameter space of all of the
 -- interesting values for the operands.  Examine all of the locations that do
