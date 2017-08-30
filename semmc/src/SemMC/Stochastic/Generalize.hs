ba7e4d84125d9ddfa2892927959d60a7304f99ea
Tristan Ravitch <tristan@nochair.net>
2017-08-30 16:53:21 -0700

    Document generalize (and note why we aren't implementing it yet)
    It is only useful for x86_64 (maybe not even then), and at the very least isn't
    applicable to the other architectures we care about for now.

--- semmc/src/SemMC/Stochastic/Generalize.hs
+++ semmc/src/SemMC/Stochastic/Generalize.hs
@@ -8,7 +8,14 @@ import qualified SemMC.Formula as F
 import           SemMC.Symbolic ( Sym )
 import SemMC.Stochastic.Monad
 
+-- | Attempt to generalize over different immediate sizes.
+--
+-- This is really only important for x86(_64).  For other architectures,
+-- operands are always the same size.  In our eventual representation of
+-- x86(_64) instructions, it may be the case that immediates with different
+-- sizes/extension properties will be represented by different opcodes.
+--
+-- For now, this function is a no-op and just returns all of the learned
+-- formulas.
 generalize :: Syn t arch (MapF.MapF (Opcode arch (Operand arch)) (F.ParameterizedFormula (Sym t) arch))
-generalize = do
-  _ <- undefined
-  askFormulas
+generalize = askFormulas
