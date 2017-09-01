5edd6a6c7c0cfd8d48f1b1fb4096a53a8b821464
Tristan Ravitch <tristan@nochair.net>
2017-09-01 14:06:28 -0700

    Implement 'removeInvalidPrograms'

--- semmc/src/SemMC/Stochastic/Classify.hs
+++ semmc/src/SemMC/Stochastic/Classify.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module SemMC.Stochastic.Classify (
@@ -10,27 +11,34 @@ module SemMC.Stochastic.Classify (
   countPrograms
   ) where
 
+import qualified GHC.Err.Located as L
+
 import           Control.Monad.Trans ( liftIO )
 import qualified Data.Foldable as F
+import qualified Data.List as L
+import qualified Data.Map.Strict as M
+import           Data.Maybe ( mapMaybe )
 import qualified Data.Set as S
+import           Data.Word ( Word64 )
+import           Text.Printf ( printf )
 
-import           Data.Parameterized.Classes ( OrdF )
-import           SemMC.Architecture ( Operand )
+import qualified SemMC.Architecture as A
 import qualified SemMC.Formula as F
 import qualified SemMC.Formula.Equivalence as F
 import qualified SemMC.Formula.Instantiate as F
+import qualified SemMC.Concrete.Execution as CE
 import qualified SemMC.Concrete.State as CS
 import           SemMC.Symbolic ( Sym )
 import           SemMC.Stochastic.Monad
-import           SemMC.Stochastic.Pseudo ( ArchitectureWithPseudo, SynthInstruction(..) )
+import qualified SemMC.Stochastic.Pseudo as P
 
 -- | A set of equivalence classes of programs
-data EquivalenceClasses arch = EquivalenceClasses { unClasses :: S.Set (S.Set [SynthInstruction arch]) }
+data EquivalenceClasses arch = EquivalenceClasses { unClasses :: S.Set (S.Set [P.SynthInstruction arch]) }
 
-data EquivalenceClass arch = EquivalenceClass { unClass :: S.Set [SynthInstruction arch] }
+data EquivalenceClass arch = EquivalenceClass { unClass :: S.Set [P.SynthInstruction arch] }
 
 -- | Construct an initial set of equivalence classes with a single program
-equivalenceClasses :: [SynthInstruction arch] -> EquivalenceClasses arch
+equivalenceClasses :: [P.SynthInstruction arch] -> EquivalenceClasses arch
 equivalenceClasses p = EquivalenceClasses (S.singleton (S.singleton p))
 
 -- | Count the total number of programs in the set of equivalence classes
@@ -44,10 +52,10 @@ countPrograms s = sum (map S.size (S.toList (unClasses s)))
 -- This function returns 'Nothing' if the new program generates a counterexample
 -- that invalidates all of the previous programs.
 classify :: forall arch t
-          . (ArchitectureWithPseudo arch, OrdF (Operand arch))
+          . (SynC arch)
          => CS.RegisterizedInstruction arch
          -- ^ The instruction whose semantics we are trying to learn
-         -> [SynthInstruction arch]
+         -> [P.SynthInstruction arch]
          -- ^ The program we just learned and need to add to an equivalence class
          -> EquivalenceClasses arch
          -- ^ The current equivalence classes
@@ -74,16 +82,16 @@ classify target p eqclasses = do
 --
 -- This function also produces counterexamples if the new program isn't
 -- equivalent to the old programs.
-classifyByClass :: (ArchitectureWithPseudo arch, Ord (SynthInstruction arch))
+classifyByClass :: (SynC arch)
                 => CS.RegisterizedInstruction arch
                 -- ^ The target instruction
-                -> [SynthInstruction arch]
+                -> [P.SynthInstruction arch]
                 -- ^ The program to classify
-                -> S.Set (S.Set [SynthInstruction arch])
+                -> S.Set (S.Set [P.SynthInstruction arch])
                 -- ^ The set of classes matching the input program
-                -> [S.Set [SynthInstruction arch]]
+                -> [S.Set [P.SynthInstruction arch]]
                 -- ^ The existing equivalence classes
-                -> Syn t arch (Maybe (S.Set (S.Set [SynthInstruction arch])))
+                -> Syn t arch (Maybe (S.Set (S.Set [P.SynthInstruction arch])))
 classifyByClass target p eqs klasses =
   case klasses of
     [] -> return (Just eqs)
@@ -93,8 +101,10 @@ classifyByClass target p eqs klasses =
       case eqv of
         F.Equivalent -> classifyByClass target p (S.insert klass eqs) rest
         F.DifferentBehavior cx -> do
-          addTestCase cx
-          eqclasses' <- removeInvalidPrograms target cx undefined
+          -- See Note [Registerization and Counterexamples]
+          let (target', cx') = CS.registerizeInstruction target cx
+          addTestCase cx'
+          eqclasses' <- removeInvalidPrograms target' cx' undefined
           case countPrograms eqclasses' of
             0 -> return Nothing
             -- FIXME: We are modifying eqclasses while we iterate over them.
@@ -107,11 +117,73 @@ classifyByClass target p eqs klasses =
 -- We have a new counterexample and need to do concrete executions of *each*
 -- existing candidate programs and reject those that do not provide the correct
 -- output.
-removeInvalidPrograms :: CS.RegisterizedInstruction arch
+removeInvalidPrograms :: (SynC arch)
+                      => A.Instruction arch
                       -> CS.ConcreteState arch
                       -> EquivalenceClasses arch
                       -> Syn t arch (EquivalenceClasses arch)
-removeInvalidPrograms target cx (EquivalenceClasses klasses) = undefined
+removeInvalidPrograms target cx (EquivalenceClasses klasses) = do
+  targetTC <- mkTestCase cx [target]
+  CE.TestSuccess (CE.TestResult { CE.resultContext = targetSt })
+    <- runConcreteTest targetTC
+  -- Convert the equivalence classes to nested lists, then tag each program with
+  -- a number.  Keep the proper nesting structure, though, so that we don't
+  -- destroy the classes.
+  let testLists = numberNestedListItems [ F.toList klass | klass <- F.toList klasses ]
+  (testCases, testIndex) <- F.foldrM (makeAndIndexTest cx) ([], M.empty) (concat testLists)
+  testResults <- runConcreteTests testCases
+  let testLists' = [ S.fromList (mapMaybe (consistentWithTarget testIndex testResults targetSt) testList)
+                   | testList <- testLists
+                   ]
+  return $ EquivalenceClasses (S.fromList testLists')
+
+-- | Reject candidate programs that have a different behavior on the given counterexample
+consistentWithTarget :: (SynC arch)
+                     => M.Map Int Word64
+                     -- ^ The mapping of candidate program numbers to test
+                     -- nonces (so that we can find the results for a given
+                     -- candidate program)
+                     -> CE.ResultIndex (CS.ConcreteState arch)
+                     -- ^ All of the results that came back from the remote runner
+                     -> CS.ConcreteState arch
+                     -- ^ The output of the target instruction on the test vector
+                     -> (Int, [P.SynthInstruction arch])
+                     -- ^ The current program (and its index into the @testIndex@ map)
+                     -> Maybe [P.SynthInstruction arch]
+consistentWithTarget testIndex testResults cx (testNum, tc) =
+  case M.lookup nonce (CE.riExitedWithSignal testResults) of
+    Just _ -> Nothing
+    Nothing ->
+      case M.lookup nonce (CE.riSuccesses testResults) of
+        Nothing -> L.error (printf "Missing a test result for test number %d with nonce %d" testNum nonce)
+        Just res
+          | CE.resultContext res == cx -> Just tc
+          | otherwise -> Nothing
+  where
+    Just nonce = M.lookup testNum testIndex
+
+-- | Create a test case out of a test vector and the given candidate program.
+--
+-- We have to expand the 'SynthInstruction's to real instructions before we
+-- construct the test case.  We accumulate the test cases and maintain a mapping
+-- between test numbers and concrete test nonces.
+makeAndIndexTest :: (P.ArchitectureWithPseudo arch)
+                 => CS.ConcreteState arch
+                 -> (Int, [P.SynthInstruction arch])
+                 -> ([CE.TestCase (CS.ConcreteState arch) (A.Instruction arch)], M.Map Int Word64)
+                 -> Syn t arch ([CE.TestCase (CS.ConcreteState arch) (A.Instruction arch)], M.Map Int Word64)
+makeAndIndexTest cx (pix, insns) (cases, idx) = do
+  tc <- mkTestCase cx program
+  return (tc : cases, M.insert pix (CE.testNonce tc) idx)
+  where
+    program = concatMap P.synthInsnToActual insns
+
+-- | Assign a unique number to each list item (in the inner lists)
+numberNestedListItems :: [[a]] -> [[(Int, a)]]
+numberNestedListItems = snd . L.mapAccumR number1 0
+  where
+    number1 n = L.mapAccumR number2 n
+    number2 n itm = (n + 1, (n, itm))
 
 -- | Heuristically-choose the best equivalence class
 --
@@ -123,7 +195,7 @@ chooseClass = undefined
 --
 -- We want to minimize the number of uninterpreted functions and non-linear
 -- operations (as well as formula size).
-chooseProgram :: EquivalenceClass arch -> Syn t arch [SynthInstruction arch]
+chooseProgram :: EquivalenceClass arch -> Syn t arch [P.SynthInstruction arch]
 chooseProgram = undefined
 
 -- | Flatten a set of equivalence classes into one equivalence class
@@ -133,21 +205,21 @@ mergeClasses :: (Ord pgm)
 mergeClasses s = S.unions (S.toList s)
 
 -- | Convert an instruction into a 'F.Formula'
-instructionFormula :: (ArchitectureWithPseudo arch)
+instructionFormula :: (P.ArchitectureWithPseudo arch)
                    => Sym t
-                   -> SynthInstruction arch
+                   -> P.SynthInstruction arch
                    -> Syn t arch (F.Formula (Sym t) arch)
 instructionFormula sym i = do
   case i of
-    SynthInstruction op operands -> do
+    P.SynthInstruction op operands -> do
       Just pf <- lookupFormula op
       (_, f) <- liftIO $ F.instantiateFormula sym pf operands
       return f
 
 -- | Convert a program into a formula
-programFormula :: (ArchitectureWithPseudo arch)
+programFormula :: (P.ArchitectureWithPseudo arch)
                => Sym t
-               -> [SynthInstruction arch]
+               -> [P.SynthInstruction arch]
                -> Syn t arch (F.Formula (Sym t) arch)
 programFormula sym insns = do
   fs <- mapM (instructionFormula sym) insns
@@ -156,9 +228,9 @@ programFormula sym insns = do
 -- | Use an SMT solver to check if two programs are equivalent.
 --
 -- If they are not, return an input that demonstrates the difference.
-testEquivalence :: (ArchitectureWithPseudo arch)
-                => [SynthInstruction arch]
-                -> [SynthInstruction arch]
+testEquivalence :: (P.ArchitectureWithPseudo arch)
+                => [P.SynthInstruction arch]
+                -> [P.SynthInstruction arch]
                 -> Syn t arch (F.EquivalenceResult arch CS.Value)
 testEquivalence p representative = do
   withSymBackend $ \sym -> do
@@ -166,3 +238,15 @@ testEquivalence p representative = do
     repFormula <- programFormula sym representative
     liftIO $ F.formulasEquivConcrete sym pf repFormula
 
+
+{- Note [Registerization and Counterexamples]
+
+  FIXME: If cx /= cx', we don't know if this is still a counterexample.  That
+  would be kind of strange, but we need to be able to handle that case.
+
+  Maybe we tweak the counterexample at a higher level (when we generate it)
+  and add that as a test case.  If it is still a counterexample, throwing
+  away candidate programs is still sound.
+
+
+-}
