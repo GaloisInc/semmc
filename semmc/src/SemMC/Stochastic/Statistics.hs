60cb2e41065e68983f08d1216e7bb321d2c13864
Tristan Ravitch <tristan@nochair.net>
2017-09-13 23:22:21 -0700

    Add a statistics persistence backend
    It is based on sqlite-simple right now.  It isn't quite complete - it needs to
    implement "insert or get the identifier for an opcode" function (opcodeId).

--- semmc/src/SemMC/Stochastic/Statistics.hs
+++ semmc/src/SemMC/Stochastic/Statistics.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 -- | A module defining some helpers to manage run-time statistics
 --
 -- It is structured as its own thread so that access to the on-disk stats files
@@ -21,39 +23,17 @@ module SemMC.Stochastic.Statistics (
 
 import qualified Control.Concurrent.Async as A
 import qualified Control.Concurrent.STM as STM
-import qualified Data.Map.Strict as M
+import qualified Control.Exception as E
+import           Data.Proxy ( Proxy(..) )
 import qualified Data.Time.Clock as TM
-import           Data.Word ( Word64 )
+import qualified Database.SQLite.Simple as SQL
+import qualified Database.SQLite.Simple.FromField as SQL
+import qualified Database.SQLite.Simple.ToField as SQL
 
-import           Data.Parameterized.Classes ( OrdF )
+import           Data.Parameterized.Classes ( OrdF, ShowF(..) )
 import           Data.Parameterized.Some ( Some(..) )
 
 import           SemMC.Architecture ( Opcode, Operand )
-
-data Statistics arch =
-  Statistics { sSolverInvocations :: Word64
-             -- ^ The overall number of solver invocations
-             , sSolverTime :: Word64
-             -- ^ The cumulative number of milliseconds of solver time
-             , sSolverTimeouts :: Word64
-             -- ^ The number of timeouts
-             , sOpcodeTime :: M.Map (Some (Opcode arch (Operand arch))) Word64
-             -- ^ The time required to learn the semantics for each operand
-             , sOpcodeTimeouts :: M.Map (Some (Opcode arch (Operand arch))) Word64
-             -- ^ The number of timeouts for each opcode
-             , sConcreteExecutions :: Word64
-             -- ^ The number of concrete program executions performed (of target
-             -- programs and candidate programs)
-             }
-
-emptyStats :: Statistics arch
-emptyStats = Statistics { sSolverInvocations = 0
-                        , sSolverTime = 0
-                        , sSolverTimeouts = 0
-                        , sOpcodeTime = M.empty
-                        , sOpcodeTimeouts = M.empty
-                        , sConcreteExecutions = 0
-                        }
 
 data StatisticsRecord arch = Terminate
                            -- ^ Tell the thread to terminate
@@ -69,50 +49,103 @@ data StatisticsRecord arch = Terminate
 data StatisticsThread arch =
   StatisticsThread { stMsgs :: STM.TChan (StatisticsRecord arch)
                    , stTerm :: STM.TChan ()
-                   , stStats :: STM.TVar (Statistics arch)
-                   , stFilePath :: FilePath
+                   , stConn :: SQL.Connection
                    }
+
+initializeSchema :: SQL.Connection -> IO ()
+initializeSchema conn = mapM_ (SQL.execute_ conn) schema
+
+-- | Look up the ID for an opcode (adding it to the database if required)
+opcodeId :: (ShowF (Opcode arch (Operand arch)))
+         => proxy arch
+         -> SQL.Connection
+         -> Some (Opcode arch (Operand arch))
+         -> IO OpcodeId
+opcodeId _ conn (Some opc) = undefined
 
 -- | Start a new statistics thread
 --
 -- The 'FilePath' describes the location on disk to persist statistics after
 -- updates.
-newStatisticsThread :: (OrdF (Opcode arch (Operand arch)))
+newStatisticsThread :: (OrdF (Opcode arch (Operand arch)), ShowF (Opcode arch (Operand arch)))
                     => FilePath
                     -> IO (StatisticsThread arch)
 newStatisticsThread statsFile = do
   chan <- STM.newTChanIO
   term <- STM.newTChanIO
-  stats <- STM.newTVarIO emptyStats
-  -- FIXME: Load up an initial stats file
+  conn <- SQL.open statsFile
+  initializeSchema conn
   let st = StatisticsThread { stMsgs = chan
                             , stTerm = term
-                            , stStats = stats
-                            , stFilePath = statsFile
+                            , stConn = conn
                             }
-  _a <- A.async (loop st)
+  _a <- A.async (loop st `E.finally` SQL.close conn)
   return st
 
-loop :: (OrdF (Opcode arch (Operand arch))) => StatisticsThread arch -> IO ()
+newtype OpcodeId = OpcodeId { unOpcodeId :: Int }
+  deriving (Show)
+
+insertSynthesizeSuccess :: SQL.Connection -> OpcodeId -> TM.NominalDiffTime -> IO ()
+insertSynthesizeSuccess conn oid tm =
+  SQL.execute conn "INSERT INTO synthesize_success VALUES(?,?);" (oid, realToFrac tm :: Double)
+
+insertStrataSuccess :: SQL.Connection -> OpcodeId -> TM.NominalDiffTime -> IO ()
+insertStrataSuccess conn oid tm =
+  SQL.execute conn "INSERT INTO strata_success VALUES(?,?);" (oid, realToFrac tm :: Double)
+
+insertCounterexample :: SQL.Connection -> OpcodeId -> IO ()
+insertCounterexample conn oid =
+  SQL.execute conn "INSERT INTO counterexample_found VALUES(?);" (SQL.Only oid)
+
+insertStrataTimeout :: SQL.Connection -> OpcodeId -> IO ()
+insertStrataTimeout conn oid =
+  SQL.execute conn "INSERT INTO strata_timeouts VALUES(?);" (SQL.Only oid)
+
+insertSolverSuccess :: SQL.Connection -> OpcodeId -> TM.NominalDiffTime -> IO ()
+insertSolverSuccess conn oid tm =
+  SQL.execute conn "INSERT INTO solver_invocation_success VALUES(?,?);" (oid, realToFrac tm :: Double)
+
+insertSolverTimeout :: SQL.Connection -> OpcodeId -> IO ()
+insertSolverTimeout conn oid =
+  SQL.execute conn "INSERT INTO solver_invocation_timeout VALUES(?);" (SQL.Only oid)
+
+insertRemovedCandidates :: SQL.Connection -> OpcodeId -> Int -> IO ()
+insertRemovedCandidates conn oid nRemoved =
+  SQL.execute conn "INSERT INTO removed_candidate_programs VALUES(?,?);" (oid, nRemoved)
+
+loop :: forall arch
+      . (OrdF (Opcode arch (Operand arch)), ShowF (Opcode arch (Operand arch)))
+     => StatisticsThread arch
+     -> IO ()
 loop st = do
   msg <- STM.atomically $ STM.readTChan (stMsgs st)
   case msg of
     Terminate -> STM.atomically $ STM.writeTChan (stTerm st) ()
-    SolverInvocation op tag@(asMilliseconds -> millis) -> do
-      stats <- STM.atomically $ do
-        stats <- STM.readTVar (stStats st)
-        let stats' = stats { sSolverInvocations = sSolverInvocations stats + 1
-                           , sSolverTime = sSolverTime stats + millis
-                           , sOpcodeTime = M.insertWith (+) op millis (sOpcodeTime stats)
-                           }
-            stats'' = case tag of
-                        Completed {} -> stats'
-                        Timeout _ -> stats' { sSolverTimeouts = sSolverTimeouts stats' + 1
-                                            , sOpcodeTimeouts = M.insertWith (+) op 1 (sOpcodeTimeouts stats')
-                                            }
-        STM.writeTVar (stStats st) stats''
-        return stats''
-      writeStatsFile (stFilePath st) stats
+    StrataTimeout sop -> do
+      oid <- opcodeId (Proxy @arch) (stConn st) sop
+      insertStrataTimeout (stConn st) oid
+      loop st
+    SolverInvocation sop tm -> do
+      oid <- opcodeId (Proxy @arch) (stConn st) sop
+      case tm of
+        Completed dt -> insertSolverSuccess (stConn st) oid dt
+        Timeout _ -> insertSolverTimeout (stConn st) oid
+      loop st
+    RemovedCandidatePrograms sop nRemoved -> do
+      oid <- opcodeId (Proxy @arch) (stConn st) sop
+      insertRemovedCandidates (stConn st) oid nRemoved
+      loop st
+    CounterexampleFound sop -> do
+      oid <- opcodeId (Proxy @arch) (stConn st) sop
+      insertCounterexample (stConn st) oid
+      loop st
+    StrataSuccess sop tm -> do
+      oid <- opcodeId (Proxy @arch) (stConn st) sop
+      insertStrataSuccess (stConn st) oid tm
+      loop st
+    SynthesizeSuccess sop tm -> do
+      oid <- opcodeId (Proxy @arch) (stConn st) sop
+      insertSynthesizeSuccess (stConn st) oid tm
       loop st
 
 -- | Send a message to terminate the statistics thread and wait for a response
@@ -127,18 +160,12 @@ terminateStatisticsThread st = do
 data SolverTime = Timeout TM.NominalDiffTime
                 | Completed TM.NominalDiffTime
 
-asMilliseconds :: SolverTime -> Word64
-asMilliseconds st =
-  case st of
-    Timeout w -> round (w * 1000)
-    Completed w -> round (w * 1000)
-
 -- | Record a single invocation of the solver (on behalf of some instruction)
 -- and the time taken for the invocation
 recordSolverInvocation :: Some (Opcode arch (Operand arch)) -> SolverTime -> StatisticsThread arch -> IO ()
-recordSolverInvocation op millis st = STM.atomically $ STM.writeTChan (stMsgs st) msg
+recordSolverInvocation op tm st = STM.atomically $ STM.writeTChan (stMsgs st) msg
   where
-    msg = SolverInvocation op millis
+    msg = SolverInvocation op tm
 
 recordCounterexample :: Some (Opcode arch (Operand arch)) -> StatisticsThread arch -> IO ()
 recordCounterexample op st =
@@ -160,5 +187,32 @@ recordRemovedCandidatePrograms :: Some (Opcode arch (Operand arch)) -> Int -> St
 recordRemovedCandidatePrograms op nRemoved st =
   STM.atomically $ STM.writeTChan (stMsgs st) (RemovedCandidatePrograms op nRemoved)
 
-writeStatsFile :: FilePath -> Statistics arch -> IO ()
-writeStatsFile = undefined
+
+instance SQL.ToField OpcodeId where
+  toField = SQL.toField . unOpcodeId
+
+instance SQL.FromField OpcodeId where
+  fromField x = OpcodeId <$> SQL.fromField x
+
+schema :: [SQL.Query]
+schema = [ "CREATE TABLE IF NOT EXISTS opcodes(opid INTEGER PRIMARY KEY,\
+                                              \name TEXT NOT NULL UNIQUE);"
+         , "CREATE TABLE IF NOT EXISTS strata_timeouts(strata_timeout_opid INTEGER NOT NULL,\
+                                                      \FOREIGN_KEY(strata_timeout_opid) REFERENCES (opid));"
+         , "CREATE TABLE IF NOT EXISTS solver_invocation_success(solver_invocation_opid INTEGER NOT NULL,\
+                                                                \success_seconds REAL NOT NULL,\
+                                                                \FOREIGN_KEY(solver_invocation_opid) REFERENCES (opid));"
+         , "CREATE TABLE IF NOT EXISTS solver_invocation_timeout(solver_invocation_opid INTEGER NOT NULL,\
+                                                                \FOREIGN_KEY(solver_invocation_opid) REFERENCES (opid));"
+         , "CREATE TABLE IF NOT EXISTS removed_candidate_programs(remove_candidate_programs_opid INTEGER NOT NULL,\
+                                                                 \num_removed INTEGER NOT NULL,\
+                                                                 \FOREIGN_KEY(remove_candidate_programs_opid) REFERENCES(opid));"
+         , "CREATE TABLE IF NOT EXISTS counterexample_found(counterexample_found_opid INTEGER NOT NULL,\
+                                                           \FOREIGN_KEY(counterexample_found_opid) REFERENCES (opid));"
+         , "CREATE TABLE IF NOT EXISTS strata_success(strata_success_opid INTEGER NOT NULL,\
+                                                     \strata_seconds REAL NOT NULL,\
+                                                     \FOREIGN_KEY(strata_success_opid) REFERENCES(opid));"
+         , "CREATE TABLE IF NOT EXISTS synthesize_success(synthesize_success_opid INTEGER NOT NULL,\
+                                                         \synthesize_seconds REAL NOT NULL,\
+                                                         \FOREIGN_KEY(synthesize_success_opid) REFERENCES(opid));"
+         ]
