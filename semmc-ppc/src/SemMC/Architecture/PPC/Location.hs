81309f7e59c8dd52094af1d8caaf0ab46e92e6e5
Tristan Ravitch <tristan@nochair.net>
2017-10-09 11:26:13 -0700

    [ppc] Remove some unused imports

--- semmc-ppc/src/SemMC/Architecture/PPC/Location.hs
+++ semmc-ppc/src/SemMC/Architecture/PPC/Location.hs
@@ -18,19 +18,11 @@ import qualified Data.Parameterized.Ctx as Ctx
 import           Data.Parameterized.Classes
 import           Data.Parameterized.NatRepr
 import           Data.Parameterized.TH.GADT
-import           Data.Parameterized.Some
-import qualified Text.Megaparsec as P
-import qualified Text.Megaparsec.Char as P
 import           Text.PrettyPrint.HughesPJClass ( pPrint )
 
 import           Lang.Crucible.BaseTypes
-import qualified Lang.Crucible.Solver.Interface as S
 
 import qualified Dismantle.PPC as PPC
-
-import qualified SemMC.Architecture as A
-
-import qualified SemMC.Architecture.PPC.Shared as PPCS
 
 type family ArchRegWidth arch :: Nat
 
