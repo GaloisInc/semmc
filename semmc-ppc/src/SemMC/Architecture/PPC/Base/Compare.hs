d5a8ed7cf95f740d6b78b0017aad6064e4b1a480
Ben Selfridge <benselfridge@galois.com>
2017-11-28 16:27:08 -0800

    Deleted extraneous definition for CMPLW semantics

--- semmc-ppc/src/SemMC/Architecture/PPC/Base/Compare.hs
+++ semmc-ppc/src/SemMC/Architecture/PPC/Base/Compare.hs
@@ -40,7 +40,7 @@ baseCompare = do
     let newCR = cmpImm bvslt bvsgt (Loc fld) ximm (sext lowreg)
     defLoc cr newCR
   defineOpcodeWithIP "CMPLW" $ do
-    comment "Compare (X-form)"
+    comment "Compare Logical (X-form)"
     comment "Compare word (where L=0)"
     fld <- param "fld" crrc (EBV 3)
     rA <- param "rA" gprc naturalBV
@@ -51,7 +51,7 @@ baseCompare = do
     input xer
     let lowa = lowBits 32 (Loc rA)
     let lowb = lowBits 32 (Loc rB)
-    let newCR = cmpImm bvslt bvsgt (Loc fld) (zext lowa) (zext lowb)
+    let newCR = cmpImm bvult bvugt (Loc fld) (zext lowa) (zext lowb)
     defLoc cr newCR
   defineOpcodeWithIP "CMPLWI" $ do
     comment "Compare Logical Immediate (D-form)"
@@ -66,20 +66,6 @@ baseCompare = do
     let ximm = zext (Loc imm)
     let lowreg = lowBits 32 (Loc rA)
     let newCR = cmpImm bvult bvugt (Loc fld) ximm (zext lowreg)
-    defLoc cr newCR
-  defineOpcodeWithIP "CMPLW" $ do
-    comment "Compare Logical (X-form)"
-    comment "Compare word (where L=0)"
-    fld <- param "fld" crrc (EBV 3)
-    rA <- param "rA" gprc naturalBV
-    rB <- param "rB" gprc naturalBV
-    input rA
-    input rB
-    input cr
-    input xer
-    let lowa = lowBits 32 (Loc rA)
-    let lowb = lowBits 32 (Loc rB)
-    let newCR = cmpImm bvult bvugt (Loc fld) (zext lowa) (zext lowb)
     defLoc cr newCR
 
   defineOpcodeWithIP "CMPB" $ do
