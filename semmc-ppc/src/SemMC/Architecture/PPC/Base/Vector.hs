39345e7f8521a9386ba256d18ad7310bff31baf8
Ben Selfridge <benselfridge@galois.com>
2017-12-29 14:27:18 -0800

    Added ppc.vec UFs; still need to pass opcode to it.

--- semmc-ppc/src/SemMC/Architecture/PPC/Base/Vector.hs
+++ semmc-ppc/src/SemMC/Architecture/PPC/Base/Vector.hs
@@ -5,9 +5,39 @@ module SemMC.Architecture.PPC.Base.Vector (
   baseVector
   ) where
 
+import GHC.Stack ( HasCallStack )
 import Prelude hiding ( concat )
 import SemMC.DSL
 import SemMC.Architecture.PPC.Base.Core
+
+import Data.Parameterized.Some
+
+ppcvec1 :: (HasCallStack)
+        => String
+        -> Expr 'TBV
+        -> Expr 'TBV
+        -> Expr 'TBV
+ppcvec1 name vr1 vfpscr =
+  uf (EBV 160) "ppc.vec1" [ Some vr1, Some vfpscr ]
+
+ppcvec2 :: (HasCallStack)
+        => String
+        -> Expr 'TBV
+        -> Expr 'TBV
+        -> Expr 'TBV
+        -> Expr 'TBV
+ppcvec2 name vr1 vr2 vfpscr =
+  uf (EBV 160) "ppc.vec2" [ Some vr1, Some vr2, Some vfpscr ]
+
+ppcvec3 :: (HasCallStack)
+        => String
+        -> Expr 'TBV
+        -> Expr 'TBV
+        -> Expr 'TBV
+        -> Expr 'TBV
+        -> Expr 'TBV
+ppcvec3 name vr1 vr2 vr3 vfpscr =
+  uf (EBV 160) "ppc.vec3" [ Some vr1, Some vr2, Some vr3, Some vfpscr ]
 
 -- | Definitions of vector instructions
 --
@@ -26,14 +56,19 @@ baseVector = do
 
   defineOpcodeWithIP "VPERM" $ do
     comment "Vector Permute (VA-form)"
-    (vrT, _, _, _) <- vaform
-    defLoc vrT (undefinedBV 128)
+    (vrT, vrA, vrB, vrC) <- vaform
+    input fpscr
+    let res = ppcvec3 "VPERM" (Loc vrA) (Loc vrB) (Loc vrC) (Loc fpscr)
+    defLoc vrT (highBits' 128 res)
+    defLoc fpscr (lowBits' 32 res)
 
   defineOpcodeWithIP "VSEL" $ do
     comment "Vector Select (VA-form)"
-    (vrT, _, _, _) <- vaform
-    defLoc vrT (undefinedBV 128)
-
+    (vrT, vrA, vrB, vrC) <- vaform
+    input fpscr
+    let res = ppcvec3 "VSEL" (Loc vrA) (Loc vrB) (Loc vrC) (Loc fpscr)
+    defLoc vrT (highBits' 128 res)
+    defLoc fpscr (lowBits' 32 res)
   return ()
 
 vecCompare :: (?bitSize :: BitSize) => SemM 'Top ()
--- semmc-ppc/src/SemMC/Architecture/PPC/UF.hs
+++ semmc-ppc/src/SemMC/Architecture/PPC/UF.hs
@@ -8,7 +8,10 @@ module SemMC.Architecture.PPC.UF (
   ) where
 
 import           GHC.TypeLits
-import qualified Data.Parameterized.Context as Ctx
+import           Data.Parameterized.Context ( Assignment
+                                            , EmptyCtx
+                                            , SingleCtx
+                                            , (::>))
 import           Data.Parameterized.Some ( Some(..) )
 import           Lang.Crucible.BaseTypes
 
@@ -17,134 +20,152 @@ import           SemMC.Architecture.PPC.Location
 uninterpretedFunctions :: forall proxy ppc
                         . (KnownNat (ArchRegWidth ppc), 1 <= ArchRegWidth ppc)
                        => proxy ppc
-                       -> [(String, Some (Ctx.Assignment BaseTypeRepr), Some BaseTypeRepr)]
+                       -> [(String, Some (Assignment BaseTypeRepr), Some BaseTypeRepr)]
 uninterpretedFunctions _ =
   [ ("fp.add64",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType 64 Ctx.::> BaseBVType 64)),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType 64 ::> BaseBVType 64)),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 64)))
   , ("fp.add32",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType 32 Ctx.::> BaseBVType 32)),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType 32 ::> BaseBVType 32)),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 32)))
   , ("fp.sub64",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType 64 Ctx.::> BaseBVType 64)),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType 64 ::> BaseBVType 64)),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 64)))
   , ("fp.sub32",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType 32 Ctx.::> BaseBVType 32)),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType 32 ::> BaseBVType 32)),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 32)))
   , ("fp.mul64",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType 64 Ctx.::> BaseBVType 64)),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType 64 ::> BaseBVType 64)),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 64)))
   , ("fp.mul32",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType 32 Ctx.::> BaseBVType 32)),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType 32 ::> BaseBVType 32)),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 32)))
   , ("fp.div64",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType 64 Ctx.::> BaseBVType 64)),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType 64 ::> BaseBVType 64)),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 64)))
   , ("fp.div32",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType 32 Ctx.::> BaseBVType 32)),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType 32 ::> BaseBVType 32)),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 32)))
   , ("fp.muladd64",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType 64 Ctx.::> BaseBVType 64 Ctx.::> BaseBVType 64)),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType 64 ::> BaseBVType 64 ::> BaseBVType 64)),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 64)))
   , ("fp.muladd32",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType 32 Ctx.::> BaseBVType 32 Ctx.::> BaseBVType 32)),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType 32 ::> BaseBVType 32 ::> BaseBVType 32)),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 32)))
   , ("fp.negate64",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType 64)),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType 64)),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 64)))
   , ("fp.negate32",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType 32)),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType 32)),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 32)))
   -- Round a double precision floating point value to single precision
   , ("fp.round_single",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType 64)),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType 64)),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 32)))
   -- Convert a single precision floating point value to double precision
   , ("fp.single_to_double",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType 32)),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType 32)),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 64)))
   , ("fp.abs",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType 64)),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType 64)),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 64)))
   , ("fp.is_qnan32",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType 32)),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType 32)),
      Some (knownRepr :: BaseTypeRepr BaseBoolType))
   , ("fp.is_qnan64",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType 64)),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType 64)),
      Some (knownRepr :: BaseTypeRepr BaseBoolType))
   , ("fp.is_snan32",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType 32)),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType 32)),
      Some (knownRepr :: BaseTypeRepr BaseBoolType))
   , ("fp.is_snan64",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType 64)),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType 64)),
      Some (knownRepr :: BaseTypeRepr BaseBoolType))
   , ("fp.lt",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType 64 Ctx.::> BaseBVType 64)),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType 64 ::> BaseBVType 64)),
      Some (knownRepr :: BaseTypeRepr BaseBoolType))
   , ("ppc.is_r0",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType (ArchRegWidth ppc))),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType (ArchRegWidth ppc))),
      Some (knownRepr :: BaseTypeRepr BaseBoolType))
   , ("ppc.memri_reg",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType (ArchRegWidth ppc))),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType (ArchRegWidth ppc))),
      Some (knownRepr :: BaseTypeRepr (BaseBVType (ArchRegWidth ppc))))
   , ("ppc.memri_offset",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType (ArchRegWidth ppc))),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType (ArchRegWidth ppc))),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 16)))
   , ("ppc.memrix_reg",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType (ArchRegWidth ppc))),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType (ArchRegWidth ppc))),
      Some (knownRepr :: BaseTypeRepr (BaseBVType (ArchRegWidth ppc))))
   , ("ppc.memrix_offset",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType (ArchRegWidth ppc))),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType (ArchRegWidth ppc))),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 14)))
   , ("ppc.memrr_base",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType (ArchRegWidth ppc))),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType (ArchRegWidth ppc))),
      Some (knownRepr :: BaseTypeRepr (BaseBVType (ArchRegWidth ppc))))
   , ("ppc.memrr_offset",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType (ArchRegWidth ppc))),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType (ArchRegWidth ppc))),
      Some (knownRepr :: BaseTypeRepr (BaseBVType (ArchRegWidth ppc))))
+  , ("ppc.vec1",
+     Some (knownRepr :: Assignment BaseTypeRepr ( EmptyCtx
+                                                  ::> BaseBVType 128
+                                                  ::> BaseBVType 32)),
+     Some (knownRepr :: BaseTypeRepr (BaseBVType 160)))
+  , ("ppc.vec2",
+     Some (knownRepr :: Assignment BaseTypeRepr ( EmptyCtx
+                                                  ::> BaseBVType 128
+                                                  ::> BaseBVType 128
+                                                  ::> BaseBVType 32)),
+     Some (knownRepr :: BaseTypeRepr (BaseBVType 160)))
+  , ("ppc.vec3",
+     Some (knownRepr :: Assignment BaseTypeRepr ( EmptyCtx
+                                                  ::> BaseBVType 128
+                                                  ::> BaseBVType 128
+                                                  ::> BaseBVType 128
+                                                  ::> BaseBVType 32)),
+     Some (knownRepr :: BaseTypeRepr (BaseBVType 160)))
   , ("read_mem.8",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseArrayType (Ctx.SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8) Ctx.::> BaseBVType (ArchRegWidth ppc))),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8) ::> BaseBVType (ArchRegWidth ppc))),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 8)))
   , ("read_mem.16",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseArrayType (Ctx.SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8) Ctx.::> BaseBVType (ArchRegWidth ppc))),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8) ::> BaseBVType (ArchRegWidth ppc))),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 16)))
   , ("read_mem.32",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseArrayType (Ctx.SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8) Ctx.::> BaseBVType (ArchRegWidth ppc))),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8) ::> BaseBVType (ArchRegWidth ppc))),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 32)))
   , ("read_mem.64",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseArrayType (Ctx.SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8) Ctx.::> BaseBVType (ArchRegWidth ppc))),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8) ::> BaseBVType (ArchRegWidth ppc))),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 64)))
   , ("read_mem.128",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseArrayType (Ctx.SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8) Ctx.::> BaseBVType (ArchRegWidth ppc))),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8) ::> BaseBVType (ArchRegWidth ppc))),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 128)))
   , ("write_mem.8",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseArrayType (Ctx.SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8) Ctx.::> BaseBVType (ArchRegWidth ppc) Ctx.::> BaseBVType 8)),
-     Some (knownRepr :: BaseTypeRepr (BaseArrayType (Ctx.SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8))))
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8) ::> BaseBVType (ArchRegWidth ppc) ::> BaseBVType 8)),
+     Some (knownRepr :: BaseTypeRepr (BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8))))
   , ("write_mem.16",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseArrayType (Ctx.SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8) Ctx.::> BaseBVType (ArchRegWidth ppc) Ctx.::> BaseBVType 16)),
-     Some (knownRepr :: BaseTypeRepr (BaseArrayType (Ctx.SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8))))
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8) ::> BaseBVType (ArchRegWidth ppc) ::> BaseBVType 16)),
+     Some (knownRepr :: BaseTypeRepr (BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8))))
   , ("write_mem.32",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseArrayType (Ctx.SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8) Ctx.::> BaseBVType (ArchRegWidth ppc) Ctx.::> BaseBVType 32)),
-     Some (knownRepr :: BaseTypeRepr (BaseArrayType (Ctx.SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8))))
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8) ::> BaseBVType (ArchRegWidth ppc) ::> BaseBVType 32)),
+     Some (knownRepr :: BaseTypeRepr (BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8))))
   , ("write_mem.64",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseArrayType (Ctx.SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8) Ctx.::> BaseBVType (ArchRegWidth ppc) Ctx.::> BaseBVType 64)),
-     Some (knownRepr :: BaseTypeRepr (BaseArrayType (Ctx.SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8))))
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8) ::> BaseBVType (ArchRegWidth ppc) ::> BaseBVType 64)),
+     Some (knownRepr :: BaseTypeRepr (BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8))))
   , ("write_mem.128",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseArrayType (Ctx.SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8) Ctx.::> BaseBVType (ArchRegWidth ppc) Ctx.::> BaseBVType 128)),
-     Some (knownRepr :: BaseTypeRepr (BaseArrayType (Ctx.SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8))))
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8) ::> BaseBVType (ArchRegWidth ppc) ::> BaseBVType 128)),
+     Some (knownRepr :: BaseTypeRepr (BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth ppc))) (BaseBVType 8))))
   , ("test_bit_dynamic",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType 32 Ctx.::> BaseBVType 32)),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType 32 ::> BaseBVType 32)),
      Some (knownRepr :: BaseTypeRepr BaseBoolType))
   , ("clz.32",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType 32)),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType 32)),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 32)))
   , ("clz.64",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType 64)),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType 64)),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 64)))
   , ("popcnt.32",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType 32)),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType 32)),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 32)))
   , ("popcnt.64",
-     Some (knownRepr :: Ctx.Assignment BaseTypeRepr (Ctx.EmptyCtx Ctx.::> BaseBVType 64)),
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType 64)),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 64)))
   ]
