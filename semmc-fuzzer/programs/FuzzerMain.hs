9169c8735358bc07c944f3175343946a164ac8d1
Jonathan Daugherty <jtd@galois.com>
2018-01-08 14:09:54 -0800

    fuzzer: set up nonce generator inside testRunner

--- semmc-fuzzer/programs/FuzzerMain.hs
+++ semmc-fuzzer/programs/FuzzerMain.hs
@@ -117,14 +117,13 @@ doTesting = do
           IO.exitFailure
 
   void $ C.forkIO $
-      N.withIONonceGenerator $
-          L.withLogCfg logCfg $
-              testRunner (Proxy @PPCS.PPC) opcodes caseChan resChan
+      L.withLogCfg logCfg $
+          testRunner (Proxy @PPCS.PPC) opcodes caseChan resChan
 
   L.withLogCfg logCfg $
     CE.runRemote (Just ppcRunnerFilename) hostname PPCS.testSerializer caseChan resChan
 
-testRunner :: forall proxy arch s .
+testRunner :: forall proxy arch .
               ( A.Architecture arch
               , C.ConcreteArchitecture arch
               , D.ArbitraryOperands (A.Opcode arch) (A.Operand arch)
@@ -134,51 +133,51 @@ testRunner :: forall proxy arch s .
            -> NES.Set (Some ((A.Opcode arch) (A.Operand arch)))
            -> C.Chan (Maybe [CE.TestCase (V.ConcreteState arch) (A.Instruction arch)])
            -> C.Chan (CE.ResultOrError (V.ConcreteState arch))
-           -> N.NonceGenerator IO s
            -> IO ()
-testRunner proxy opcodes caseChan resChan nonceGen = do
-  gen <- DA.createGen
+testRunner proxy opcodes caseChan resChan =
+    N.withIONonceGenerator $ \nonceGen -> do
+      gen <- DA.createGen
 
-  inst <- D.randomInstruction gen opcodes
-  state <- C.randomState proxy gen
-  nonce <- N.indexValue <$> N.freshNonce nonceGen
+      inst <- D.randomInstruction gen opcodes
+      state <- C.randomState proxy gen
+      nonce <- N.indexValue <$> N.freshNonce nonceGen
 
-  let testCase = CE.TestCase { CE.testNonce = nonce
-                             , CE.testProgram = [inst]
-                             , CE.testContext = state
-                             }
+      let testCase = CE.TestCase { CE.testNonce = nonce
+                                 , CE.testProgram = [inst]
+                                 , CE.testContext = state
+                                 }
 
-  void $ doTest Nothing testCase
-  C.writeChan caseChan Nothing
+      void $ doTest Nothing testCase
+      C.writeChan caseChan Nothing
 
-  where
-    doTest mr vec = do
-      -- Send a test case
-      C.writeChan caseChan (Just [vec])
-      -- Get the result
-      res <- C.readChan resChan
+      where
+        doTest mr vec = do
+          -- Send a test case
+          C.writeChan caseChan (Just [vec])
+          -- Get the result
+          res <- C.readChan resChan
 
-      case res of
-        CE.InvalidTag t -> do
-          L.logIO L.Error $ printf "Invalid tag: %d" t
-          IO.exitFailure
-        CE.TestContextParseFailure -> do
-          L.logIO L.Error "Test context parse failure"
-          IO.exitFailure
-        CE.TestSignalError nonce sig -> do
-          L.logIO L.Error $ printf "Failed with unexpected signal (%d) on test case %d" sig nonce
-          IO.exitFailure
-        CE.TestReadError tag -> do
-          L.logIO L.Error $ printf "Failed with a read error (%d)" tag
-          IO.exitFailure
-        CE.TestSuccess tr -> do
-          case mr of
-            Just oldRes -> do
-              when (oldRes /= CE.resultContext tr) $ do
-                L.logIO L.Error "ERROR: Context mismatch"
-            Nothing -> return ()
+          case res of
+            CE.InvalidTag t -> do
+              L.logIO L.Error $ printf "Invalid tag: %d" t
+              IO.exitFailure
+            CE.TestContextParseFailure -> do
+              L.logIO L.Error "Test context parse failure"
+              IO.exitFailure
+            CE.TestSignalError nonce sig -> do
+              L.logIO L.Error $ printf "Failed with unexpected signal (%d) on test case %d" sig nonce
+              IO.exitFailure
+            CE.TestReadError tag -> do
+              L.logIO L.Error $ printf "Failed with a read error (%d)" tag
+              IO.exitFailure
+            CE.TestSuccess tr -> do
+              case mr of
+                Just oldRes -> do
+                  when (oldRes /= CE.resultContext tr) $ do
+                    L.logIO L.Error "ERROR: Context mismatch"
+                Nothing -> return ()
 
-          return (CE.resultContext tr)
+              return (CE.resultContext tr)
 
 main :: IO ()
 main = doTesting
