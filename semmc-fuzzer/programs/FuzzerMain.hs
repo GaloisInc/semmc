4027bb609ef44d1162255ab0ee90b507c15c6c2b
Jonathan Daugherty <jtd@galois.com>
2018-01-08 14:44:42 -0800

    fuzzer: first pass at actually evaluating instructions

--- semmc-fuzzer/programs/FuzzerMain.hs
+++ semmc-fuzzer/programs/FuzzerMain.hs
@@ -6,25 +6,38 @@ module Main where
 
 import qualified Control.Concurrent as C
 import           Control.Monad (when, void)
+import qualified Data.ByteString.UTF8 as BS8
 import qualified Data.Set.NonEmpty as NES
+import           Data.EnumF (EnumF)
 import           Data.Proxy (Proxy(Proxy))
 import           Text.Printf (printf)
 import qualified System.Exit as IO
 import qualified System.IO as IO
 
-import           Data.Parameterized.Some ( Some(..) )
+import           Data.Parameterized.Some (Some(..))
 import qualified Data.Parameterized.Nonce as N
+import qualified Data.Parameterized.Map as MapF
+import           Data.Parameterized.HasRepr (HasRepr)
+import qualified Data.Parameterized.List as L
+
+import qualified Lang.Crucible.Solver.SimpleBackend as SB
 
 import qualified Dismantle.Arbitrary as DA
 import qualified Dismantle.Instruction.Random as D
 
 import qualified SemMC.Log as L
+import qualified SemMC.Formula as F
+import qualified SemMC.Concrete.Execution as CE
+import           SemMC.Architecture.Evaluate (evaluateInstruction)
 import qualified SemMC.Architecture as A
 import qualified SemMC.Architecture.Concrete as C
-import qualified SemMC.Concrete.Execution as CE
 import qualified SemMC.Architecture.View as V
 import qualified SemMC.Architecture.PPC32 as PPCS
 import           SemMC.Architecture.PPC32.Opcodes as PPCS
+import           SemMC.Synthesis.Template ( BaseSet, TemplatedArch
+                                          , unTemplate, TemplatableOperand
+                                          , TemplatedOperand
+                                          )
 
 type PPCState = V.ConcreteState PPCS.PPC
 
@@ -118,42 +131,69 @@ doTesting = do
 
   void $ C.forkIO $
       L.withLogCfg logCfg $
-          testRunner (Proxy @PPCS.PPC) opcodes caseChan resChan
+          testRunner (Proxy @PPCS.PPC) opcodes PPCS.allSemantics caseChan resChan
 
   L.withLogCfg logCfg $
     CE.runRemote (Just ppcRunnerFilename) hostname PPCS.testSerializer caseChan resChan
 
+makePlain :: forall arch sym
+           . (MapF.OrdF (A.Opcode arch (A.Operand arch)),
+              MapF.OrdF (A.Location arch))
+          => BaseSet sym arch
+          -> MapF.MapF (A.Opcode arch (A.Operand arch)) (F.ParameterizedFormula sym arch)
+makePlain = MapF.foldrWithKey f MapF.empty
+  where f :: forall sh
+           . A.Opcode arch (A.Operand arch) sh
+          -> F.ParameterizedFormula sym (TemplatedArch arch) sh
+          -> MapF.MapF (A.Opcode arch (A.Operand arch)) (F.ParameterizedFormula sym arch)
+          -> MapF.MapF (A.Opcode arch (A.Operand arch)) (F.ParameterizedFormula sym arch)
+        f op pf = MapF.insert op (unTemplate pf)
+
 testRunner :: forall proxy arch .
-              ( A.Architecture arch
+              ( TemplatableOperand arch
+              , A.Architecture arch
               , C.ConcreteArchitecture arch
               , D.ArbitraryOperands (A.Opcode arch) (A.Operand arch)
               , L.HasLogCfg
+              , MapF.OrdF (A.Opcode arch (TemplatedOperand arch))
+              , MapF.ShowF (A.Opcode arch (TemplatedOperand arch))
+              , EnumF (A.Opcode arch (TemplatedOperand arch))
+              , HasRepr (A.Opcode arch (A.Operand arch)) (L.List (A.OperandTypeRepr arch))
               )
            => proxy arch
            -> NES.Set (Some ((A.Opcode arch) (A.Operand arch)))
+           -> [(Some ((A.Opcode arch) (A.Operand arch)), BS8.ByteString)]
            -> C.Chan (Maybe [CE.TestCase (V.ConcreteState arch) (A.Instruction arch)])
            -> C.Chan (CE.ResultOrError (V.ConcreteState arch))
            -> IO ()
-testRunner proxy opcodes caseChan resChan =
+testRunner proxy opcodes semantics caseChan resChan =
     N.withIONonceGenerator $ \nonceGen -> do
-      gen <- DA.createGen
+      sym :: SB.SimpleBackend s
+          <- SB.newSimpleBackend nonceGen
 
+      baseSet <- F.loadFormulas sym semantics
+      let plainBaseSet :: MapF.MapF (A.Opcode arch (A.Operand arch)) (F.ParameterizedFormula (SB.SimpleBackend s) arch)
+          plainBaseSet = makePlain baseSet
+
+      gen <- DA.createGen
       inst <- D.randomInstruction gen opcodes
       state <- C.randomState proxy gen
       nonce <- N.indexValue <$> N.freshNonce nonceGen
+
+      expectedFinalState <- evaluateInstruction sym plainBaseSet inst state
 
       let testCase = CE.TestCase { CE.testNonce = nonce
                                  , CE.testProgram = [inst]
                                  , CE.testContext = state
                                  }
 
-      void $ doTest Nothing testCase
+      void $ doTest testCase expectedFinalState
       C.writeChan caseChan Nothing
 
       where
-        doTest mr vec = do
+        doTest tc expectedFinal = do
           -- Send a test case
-          C.writeChan caseChan (Just [vec])
+          C.writeChan caseChan (Just [tc])
           -- Get the result
           res <- C.readChan resChan
 
@@ -171,11 +211,8 @@ testRunner proxy opcodes caseChan resChan =
               L.logIO L.Error $ printf "Failed with a read error (%d)" tag
               IO.exitFailure
             CE.TestSuccess tr -> do
-              case mr of
-                Just oldRes -> do
-                  when (oldRes /= CE.resultContext tr) $ do
-                    L.logIO L.Error "ERROR: Context mismatch"
-                Nothing -> return ()
+              when (expectedFinal /= CE.resultContext tr) $ do
+                L.logIO L.Error "ERROR: Context mismatch"
 
               return (CE.resultContext tr)
 
