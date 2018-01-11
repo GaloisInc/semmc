96dd5c08a7b259fbd6c49ce7c5f62d263e66c17b
Nathan Collins <conathan@galois.com>
2018-01-10 20:13:10 -0800

    [semmc-toy] Disable rvwp optimization
    Because the pseudo formula for MovRr is not defined yet, which crashes
    STRATA after STOKE uses the optimization.

--- semmc-toy/src/SemMC/Toy.hs
+++ semmc-toy/src/SemMC/Toy.hs
@@ -541,8 +541,13 @@ allPseudoOpcodes = [ Some MovRr ]
 ----------------------------------------------------------------
 -- Rvwp optimization support
 
+-- Disable opt until pseudo instruction formula compilation is implemented.
+disableRvwpOpt :: Bool
+disableRvwpOpt = True
+
 instance R.RvwpOptimization Toy where
-  rvwpMov (V.View dstSlice (RegLoc dst)) (V.View srcSlice (RegLoc src)) = do
+  rvwpMov (V.View dstSlice (RegLoc dst)) (V.View srcSlice (RegLoc src)) | disableRvwpOpt = Nothing
+                                                                        | otherwise = do
     guard $ dst /= src
     when (not (isTrivialR32Slice dstSlice) || not (isTrivialR32Slice srcSlice)) $
       error "Toy.rvwpMov: needs to be updated for new cases that aren't handled."
