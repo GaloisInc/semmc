a27e6cedadf42ad8e0573e25a8e99afb7acd607f
Jonathan Daugherty <jtd@galois.com>
2018-01-18 13:32:35 -0800

    fuzzermon: index fields

--- semmc-fuzzer/fuzzermon/fuzzermon/main/models.py
+++ semmc-fuzzer/fuzzermon/fuzzermon/main/models.py
@@ -13,28 +13,28 @@ class Host(models.Model):
 
 class Batch(models.Model):
     fuzzer_host = models.CharField(max_length=128)
-    testing_host = models.ForeignKey(Host, on_delete=models.CASCADE)
+    testing_host = models.ForeignKey(Host, on_delete=models.CASCADE, db_index=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    submitted_at = models.DateTimeField(auto_now_add=True)
+    submitted_at = models.DateTimeField(auto_now_add=True, db_index=True)
 
 class Opcode(models.Model):
-    name = models.CharField(max_length=128)
-    arch = models.ForeignKey(Arch, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128, db_index=True)
+    arch = models.ForeignKey(Arch, on_delete=models.CASCADE, db_index=True)
 
 class TestSuccess(models.Model):
-    batch = models.ForeignKey(Batch, on_delete=models.CASCADE)
-    opcode = models.ForeignKey(Opcode, on_delete=models.CASCADE)
+    batch = models.ForeignKey(Batch, on_delete=models.CASCADE, db_index=True)
+    opcode = models.ForeignKey(Opcode, on_delete=models.CASCADE, db_index=True)
     count = models.IntegerField()
 
 class TestFailure(models.Model):
-    batch = models.ForeignKey(Batch, on_delete=models.CASCADE)
-    opcode = models.ForeignKey(Opcode, on_delete=models.CASCADE)
+    batch = models.ForeignKey(Batch, on_delete=models.CASCADE, db_index=True)
+    opcode = models.ForeignKey(Opcode, on_delete=models.CASCADE, db_index=True)
     pretty = models.CharField(max_length=256)
     arguments = models.TextField()
 
 class TestSignalError(models.Model):
-    batch = models.ForeignKey(Batch, on_delete=models.CASCADE)
-    opcode = models.ForeignKey(Opcode, on_delete=models.CASCADE)
+    batch = models.ForeignKey(Batch, on_delete=models.CASCADE, db_index=True)
+    opcode = models.ForeignKey(Opcode, on_delete=models.CASCADE, db_index=True)
     pretty = models.CharField(max_length=256)
     signal = models.IntegerField()
 
@@ -45,7 +45,7 @@ class TestSignalError(models.Model):
             return "unknown"
 
 class TestFailureState(models.Model):
-    test_failure = models.ForeignKey(TestFailure, on_delete=models.CASCADE)
+    test_failure = models.ForeignKey(TestFailure, on_delete=models.CASCADE, db_index=True)
     location = models.CharField(max_length=128)
     expected_value = models.CharField(max_length=256)
     actual_value = models.CharField(max_length=256)
