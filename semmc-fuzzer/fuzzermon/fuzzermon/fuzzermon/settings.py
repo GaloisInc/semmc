37780ebf64842188662d1b135f7ae144ac21319d
Jonathan Daugherty <jtd@galois.com>
2018-01-18 16:32:38 -0800

    fuzzermon: add sqlite3 settings for development

--- semmc-fuzzer/fuzzermon/fuzzermon/fuzzermon/settings.py
+++ semmc-fuzzer/fuzzermon/fuzzermon/fuzzermon/settings.py
@@ -74,6 +74,15 @@ WSGI_APPLICATION = 'fuzzermon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# Development:
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# Production:
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
