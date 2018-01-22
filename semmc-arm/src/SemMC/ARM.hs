7097bc02633ee455a2846c63b4c41ce718d8de53
Kevin Quick <kquick@galois.com>
2018-01-22 14:20:39 -0800

    [arm] comment-out TBD portions of the serialized remote machine state

--- semmc-arm/src/SemMC/ARM.hs
+++ semmc-arm/src/SemMC/ARM.hs
@@ -49,16 +49,17 @@ data ARM  -- arch type
 
 data MachineState =
   MachineState { gprs :: V.Vector 16 Word32
+               , pctr :: Word32  -- ^ the current Program Counter (PC)
                -- ^ 16 general purpose registers
-               , gprs_mask :: V.Vector 16 Word32
-               , fprs :: V.Vector 32 Word32
-               -- ^ 32 32-bit locations
-               , cpsr :: Word32
-               -- ^ Current program status register (CPSR)
-               , mem1 :: V.Vector 32 Word8
-               -- ^ 32 bytes
-               , mem2 :: V.Vector 32 Word8
-               -- ^ 32 bytes
+               -- , gprs_mask :: V.Vector 16 Word32
+               -- , fprs :: V.Vector 32 Word32
+               -- -- ^ 32 32-bit locations
+               -- , cpsr :: Word32
+               -- -- ^ Current program status register (CPSR)
+               -- , mem1 :: V.Vector 32 Word8
+               -- -- ^ 32 bytes
+               -- , mem2 :: V.Vector 32 Word8
+               -- -- ^ 32 bytes
                }
   deriving (Show,Eq)
 
@@ -75,11 +76,12 @@ toBS :: MachineState -> B.ByteString
 toBS ms = LB.toStrict (B.toLazyByteString bld)
   where
     bld = mconcat [ mconcat (map B.word32LE (V.toList (gprs ms)))
-                  , mconcat (map B.word32LE (V.toList (gprs_mask ms)))
-                  , mconcat (map B.word32LE (V.toList (fprs ms)))
-                  , B.word32LE (cpsr ms)
-                  , mconcat (map B.word8 (V.toList (mem1 ms)))
-                  , mconcat (map B.word8 (V.toList (mem2 ms)))
+                  , B.word32LE (pctr ms)
+                  -- , mconcat (map B.word32LE (V.toList (gprs_mask ms)))
+                  -- , mconcat (map B.word32LE (V.toList (fprs ms)))
+                  -- , B.word32LE (cpsr ms)
+                  -- , mconcat (map B.word8 (V.toList (mem1 ms)))
+                  -- , mconcat (map B.word8 (V.toList (mem2 ms)))
                   ]
 
 fromBS :: B.ByteString -> Maybe MachineState
@@ -92,19 +94,21 @@ fromBS bs =
 getMachineState :: G.Get MachineState
 getMachineState = do
   Just grs <- V.fromList <$> replicateM 16 G.getWord32le
-  -- Note that we have to parse out the mask, even though it isn't populated
-  -- here.
-  Just grs_mask <- V.fromList <$> replicateM 16 G.getWord32le
-  Just frs <- V.fromList <$> replicateM 32 G.getWord32le
-  cpsr_reg <- G.getWord32le
-  Just m1 <- V.fromList <$> replicateM 32 G.getWord8
-  Just m2 <- V.fromList <$> replicateM 32 G.getWord8
+  pcv <- G.getWord32le
+  -- -- Note that we have to parse out the mask, even though it isn't populated
+  -- -- here.
+  -- Just grs_mask <- V.fromList <$> replicateM 16 G.getWord32le
+  -- Just frs <- V.fromList <$> replicateM 32 G.getWord32le
+  -- cpsr_reg <- G.getWord32le
+  -- Just m1 <- V.fromList <$> replicateM 32 G.getWord8
+  -- Just m2 <- V.fromList <$> replicateM 32 G.getWord8
   return MachineState { gprs = grs
-                      , gprs_mask = grs_mask
-                      , fprs = frs
-                      , cpsr = cpsr_reg
-                      , mem1 = m1
-                      , mem2 = m2
+                      , pctr = pcv
+                      -- , gprs_mask = grs_mask
+                      -- , fprs = frs
+                      -- , cpsr = cpsr_reg
+                      -- , mem1 = m1
+                      -- , mem2 = m2
                       }
 
 -- ----------------------------------------------------------------------
