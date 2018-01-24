b7871bb03dfcab3d9b59082a735872edb45f6ccb
Kevin Quick <kquick@galois.com>
2018-01-23 16:15:48 -0800

    [arm] Add operand class semantics for pred and addrmode_imm12_pre.

--- semmc-arm/src/SemMC/Architecture/ARM/BaseSemantics/OperandClasses.hs
+++ semmc-arm/src/SemMC/Architecture/ARM/BaseSemantics/OperandClasses.hs
@@ -1,3 +1,7 @@
+-- | Defines the local operand references (used in the SemMC DSL
+-- specification of operands) and the corresponding operand names in
+-- Dismantle.
+
 module SemMC.Architecture.ARM.BaseSemantics.OperandClasses
     where
 
@@ -6,3 +10,10 @@ gpr = "GPR"
 
 memri :: String
 memri = "Memri"
+
+-- | pred is the 4-bit condition value that indicates whether an instruction will be executed.
+pred :: String
+pred = "Pred"
+
+addrmode_imm12_pre :: String
+addrmode_imm12_pre = "Addrmode_imm12_pre"
