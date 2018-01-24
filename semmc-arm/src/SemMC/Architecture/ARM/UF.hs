3370e7542576f936a91814ab4a65ae4c69b0d735
Kevin Quick <kquick@galois.com>
2018-01-23 22:17:42 -0800

    [arm] Add uninterpreted function declarations for memory reads/writes.

--- semmc-arm/src/SemMC/Architecture/ARM/UF.hs
+++ semmc-arm/src/SemMC/Architecture/ARM/UF.hs
@@ -33,4 +33,44 @@ uninterpretedFunctions _ =
   , ("arm.imm12_add", -- reference to U flag bit from an addrmode_imm12_pre operand
      Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType (ArchRegWidth arm))),
      Some (knownRepr :: BaseTypeRepr BaseBoolType))
+
+    -- Standard memory accesses
+
+  , ("read_mem.8",
+     Some (knownRepr :: Assignment BaseTypeRepr
+                       (EmptyCtx ::> BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth arm))) (BaseBVType 8)
+                                 ::> BaseBVType (ArchRegWidth arm))),
+     Some (knownRepr :: BaseTypeRepr (BaseBVType 8)))
+  , ("read_mem.16",
+     Some (knownRepr :: Assignment BaseTypeRepr
+                       (EmptyCtx ::> BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth arm))) (BaseBVType 8)
+                                 ::> BaseBVType (ArchRegWidth arm))),
+     Some (knownRepr :: BaseTypeRepr (BaseBVType 16)))
+  , ("read_mem.32",
+     Some (knownRepr :: Assignment BaseTypeRepr
+                       (EmptyCtx ::> BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth arm))) (BaseBVType 8)
+                                 ::> BaseBVType (ArchRegWidth arm))),
+     Some (knownRepr :: BaseTypeRepr (BaseBVType 32)))
+  , ("read_mem.64",
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth arm))) (BaseBVType 8) ::> BaseBVType (ArchRegWidth arm))),
+     Some (knownRepr :: BaseTypeRepr (BaseBVType 64)))
+  , ("read_mem.128",
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth arm))) (BaseBVType 8) ::> BaseBVType (ArchRegWidth arm))),
+     Some (knownRepr :: BaseTypeRepr (BaseBVType 128)))
+  , ("write_mem.8",
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth arm))) (BaseBVType 8) ::> BaseBVType (ArchRegWidth arm) ::> BaseBVType 8)),
+     Some (knownRepr :: BaseTypeRepr (BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth arm))) (BaseBVType 8))))
+  , ("write_mem.16",
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth arm))) (BaseBVType 8) ::> BaseBVType (ArchRegWidth arm) ::> BaseBVType 16)),
+     Some (knownRepr :: BaseTypeRepr (BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth arm))) (BaseBVType 8))))
+  , ("write_mem.32",
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth arm))) (BaseBVType 8) ::> BaseBVType (ArchRegWidth arm) ::> BaseBVType 32)),
+     Some (knownRepr :: BaseTypeRepr (BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth arm))) (BaseBVType 8))))
+  , ("write_mem.64",
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth arm))) (BaseBVType 8) ::> BaseBVType (ArchRegWidth arm) ::> BaseBVType 64)),
+     Some (knownRepr :: BaseTypeRepr (BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth arm))) (BaseBVType 8))))
+  , ("write_mem.128",
+     Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth arm))) (BaseBVType 8) ::> BaseBVType (ArchRegWidth arm) ::> BaseBVType 128)),
+     Some (knownRepr :: BaseTypeRepr (BaseArrayType (SingleCtx (BaseBVType (ArchRegWidth arm))) (BaseBVType 8))))
+
   ]
