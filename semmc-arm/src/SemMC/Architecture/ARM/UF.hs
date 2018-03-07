8d269f31ec0bff73be2d8d7c286967763c0c7a55
Kevin Quick <kquick@galois.com>
2018-03-06 23:50:01 -0800

    [arm] Fix comment reference for ThumbBlxGTarget J2 bit extraction.

--- semmc-arm/src/SemMC/Architecture/ARM/UF.hs
+++ semmc-arm/src/SemMC/Architecture/ARM/UF.hs
@@ -115,7 +115,7 @@ uninterpretedFunctions _ =
      Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType (ArchRegWidth arm))),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 1)))
 
-  , ("t32.blxtarget_J2", -- operand ThumbBlxTarget J1 bit reference
+  , ("t32.blxtarget_J2", -- operand ThumbBlxTarget J2 bit reference
      Some (knownRepr :: Assignment BaseTypeRepr (EmptyCtx ::> BaseBVType (ArchRegWidth arm))),
      Some (knownRepr :: BaseTypeRepr (BaseBVType 1)))
 
