454f798cce95cbe00ac3c27731b3ae75df9cff99
Nathan Collins <conathan@galois.com>
2018-03-20 11:26:49 -0700

    [semmc] Surface more diagnostics when checking for rvwp optimization
    When running the PPC stratify it found a candidate where the rvwp
    applied with *2* right values in the wrong places. This case is not
    implemented, and I didn't think it would ever happen, so now I need to
    understand why it's happening. However, I'm optimistic that the wrong
    places will be in different "register classes", and so I'll be handle
    them by independent movs, and still not need the xor swapping that I'd
    use if there were multiple wrong places in the same "register class".

--- semmc/src/SemMC/Stochastic/RvwpOptimization.hs
+++ semmc/src/SemMC/Stochastic/RvwpOptimization.hs
@@ -32,31 +32,27 @@ class RvwpOptimization arch where
 -- | Attempt to fix right values in the wrong places by moving them to
 -- the right place.
 --
--- Assumes that the rvwp optimization applies
+-- Assumes that the rvwp optimization applies; the input semantic
+-- views are assumed to be the output of
+-- 'checkIfRvwpOptimizationApplies'.
 fixRvwps :: (U.HasLogCfg, U.HasCallStack, RvwpOptimization arch)
-         => Int -> [V.SemanticView arch] -> [RvwpDelta arch]
+         => [V.SemanticView arch]
          -> Maybe [P.SynthInstruction arch]
-fixRvwps numRvwps outMasks rvwps
-  | numRvwps == 0 = error "fixRvwps: numRvwps == 0 so there's nothing to fix."
+fixRvwps outMasks = case outMasks of
+  [] -> error "fixRvwps: numRvwps == 0 so there's nothing to fix."
+  [mask] -> do
+    case mask of
+      V.SemanticView{..} -> do
+        let dst = semvView
+        let src = head semvCongruentViews
+        rvwpMov dst src
   -- Only implementing the simpler case of a single rv in the wp to
   -- start. The general case is more complicated to implement, and
   -- probably requires a swap instruction instead of 'rvwpMov' (if we
   -- don't assume a scratch register).
-  | numRvwps > 1  =
-    let msg = printf "fixRvpws: numRvwps > 1 is not supported; numRvwps = %i." numRvwps
-    in U.logTrace U.Error msg $ error msg
-  | otherwise = do
-      let (mask, rvwp) = head [ (m, r)
-                              | (m, r) <- zip outMasks rvwps
-                              , rdWeight r /= 0 ]
-      case mask of
-        V.SemanticView{..} -> do
-          let dst = semvView
-          let src = head [ view
-                         | (view, isRvwpPlace) <-
-                             zip semvCongruentViews (rdRvwpPlaces rvwp)
-                         , isRvwpPlace ]
-          rvwpMov dst src
+  _ -> let msg = printf "fixRvwps: numRvwps > 1 is not supported; numRvwps = %i."
+                 (length outMasks)
+       in U.logTrace U.Error msg $ error msg
 
 ----------------------------------------------------------------
 -- * Bookkeeping for rvwp optimization
@@ -87,25 +83,41 @@ combineDeltas = foldr1 (zipWith combineRvwp)
       { rdWeight = rdWeight r1 + rdWeight r2
       , rdRvwpPlaces = zipWith (&&) (rdRvwpPlaces r1) (rdRvwpPlaces r2) }
 
--- | Check if the unimplemented rvwp optimization applies.
+-- | Check if the rvwp optimization applies.
 --
 -- Returns 'Nothing' if the optimization does not apply. When the
--- optimization does apply, returns @Just k@ for @k@ the number of
--- values that occur in the wrong places.
+-- optimization does apply, returns @Just svs@ for @svs@ the
+-- @SemanticView@s for the locations whose right value occurs in the
+-- wrong place. The returned semantic views have their
+-- @semvCongruentViews@ fields filtered to only include the locations
+-- where the right value occurred.
 --
 -- The argument is suposed to be the result of 'combineDeltas' on all
 -- the delta lists for all the tests.
-checkIfRvwpOptimizationApplies :: [RvwpDelta arch] -> Maybe Int
-checkIfRvwpOptimizationApplies rvwps =
+checkIfRvwpOptimizationApplies ::
+  [V.SemanticView arch] -> [RvwpDelta arch] -> Maybe [V.SemanticView arch]
+checkIfRvwpOptimizationApplies semViews rvwps =
   if rvwpOptimizationApplies
-  then Just numRvwps
+  then Just rvwpSemanticViewsFiltered
   else Nothing
   where
     rvwpOptimizationApplies = allRightValuesAvailable &&
                               someRightValueInTheWrongPlace
     allRightValuesAvailable = or [ rdWeight == 0 || or rdRvwpPlaces
                                  | RvwpDelta{..} <- rvwps ]
-    someRightValueInTheWrongPlace = numRvwps > 0
+    someRightValueInTheWrongPlace = length rvwpSemanticViews > 0
     -- Checking for non-zero weight is only sound when
     -- 'allRightValuesAvailable' is true.
-    numRvwps = length [ () | r <- rvwps , rdWeight r /= 0 ]
+    rvwpSemanticViews = [ (s, r) | (s, r) <- zip semViews rvwps
+                                 , rdWeight r /= 0 ]
+    filterCongruentViews V.SemanticView{..} rvwp =
+      let filteredCongruentViews =
+            [ view | (view, isRvwpPlace) <-
+                     zip semvCongruentViews (rdRvwpPlaces rvwp)
+                   , isRvwpPlace ]
+      in V.SemanticView
+         { V.semvView = semvView
+         , V.semvCongruentViews = filteredCongruentViews
+         , V.semvDiff = semvDiff }
+    rvwpSemanticViewsFiltered =
+      map (uncurry filterCongruentViews) rvwpSemanticViews
