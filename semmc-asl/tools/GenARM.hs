ac96376e3033f1ae0613403008390b76107cc380
Daniel Matichuk <dmatichuk@galois.com>
2019-09-05 19:33:36 -0700

    collecting translation errors instead of throwing
    this will allow for getting an idea of the current ASL coverage

--- semmc-asl/tools/GenARM.hs
+++ semmc-asl/tools/GenARM.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
 
 module Main ( main ) where
 
@@ -9,11 +10,13 @@ import qualified Control.Exception as X
 import           Control.Monad (forM_, foldM)
 import qualified Control.Monad.State.Lazy as MSS
 import qualified Data.Map as Map
+import           Data.Maybe ( fromMaybe )
 import qualified Data.Parameterized.Context as Ctx
 import           Data.Parameterized.Nonce
 import           Data.Parameterized.Some ( Some(..) )
 import qualified Data.Text as T
 import qualified Data.List as List
+import           Data.List.Index (imap)
 import qualified Lang.Crucible.Backend.Simple as CBS
 import qualified Lang.Crucible.Backend.Online as CBO
 import qualified Lang.Crucible.FunctionHandle as CFH
@@ -38,20 +41,41 @@ defsFilePath = "test/defs.parsed"
 ignoredInstrs :: [(T.Text, T.Text)]
 ignoredInstrs =
   [("aarch32_VADDL_A", "aarch32_VADDL_T1A1_A") -- unexpected getter structure
-  ,("aarch32_QSUB16_A", "aarch32_QSUB16_A1_A") -- "R" unbound
-  ,("aarch32_QSUB16_A", "aarch32_QSUB16_T1_A") -- "R" unbound
-  ,("aarch32_SHA256H2_A", "aarch32_SHA256H2_A1_A") -- unexpected getter structure
-  ,("aarch32_SHA256H2_A","aarch32_SHA256H2_T1_A") -- unexpected getter structure
-  ,("aarch32_SEL_A", "aarch32_SEL_A1_A") -- "R" unbound
-  ,("aarch32_SEL_A", "aarch32_SEL_T1_A") -- "R" unbound
+  ,("aarch32_QSUB16_A", "aarch32_QSUB16_A1_A") -- can't slice lval for setters
+  ,("aarch32_QSUB16_A", "aarch32_QSUB16_T1_A") -- can't slice lval for setters
+  ,("aarch32_SEL_A", "aarch32_SEL_A1_A") -- can't slice lval for setters
+  ,("aarch32_SEL_A", "aarch32_SEL_T1_A") -- can't slice lval for setters
   ,("aarch32_SMLAWB_A", "aarch32_SMLAWB_A1_A") -- type mismatch
   ,("aarch32_SMLAWB_A", "aarch32_SMLAWB_T1_A") -- type mismatch
   ,("aarch32_ERET_AS", "aarch32_ERET_A1_A") -- unbound global
   ,("aarch32_ERET_AS", "aarch32_ERET_T1_A") -- unbound global
-  ,("aarch32_VPADD_f_A", "aarch32_VPADD_f_A1_A") -- unexpected getter structure
-  ,("aarch32_VPADD_f_A", "aarch32_VPADD_f_T1_A") -- unexpected getter structure
-  ,("aarch32_VSTM_A", "aarch32_VSTM_T1A1_A") -- unexpected getter structure
-  ,("aarch32_VCMP_A", "aarch32_VCMP_A1_A") -- unexpected getter structure
+  ,("aarch32_VPADD_f_A", "aarch32_VPADD_f_A1_A") -- TypeNotFound "real"
+  ,("aarch32_VCMP_A", "aarch32_VCMP_A2_A") -- TypeNotFound "real"
+  ,("aarch32_VCMP_A", "aarch32_VCMP_T1_A") -- TypeNotFound "real"
+  ,("aarch32_VCMP_A", "aarch32_VCMP_T2_A") -- TypeNotFound "real"
+  ,("aarch32_VRINTX_asimd_A", "aarch32_VRINTX_asimd_A1_A") -- TypeNotFound "real"
+  ,("aarch32_VRINTX_asimd_A", "aarch32_VRINTX_asimd_T1_A") -- TypeNotFound "real"
+  ,("aarch32_VLDR_A", "aarch32_VLDR_A1_A") -- missing "HaveFP16Ext_0"
+  ,("aarch32_VLDR_A", "aarch32_VLDR_T1_A") -- missing "HaveFP16Ext_0"
+  ,("aarch32_VORN_r_A", "aarch32_VORN_r_T1A1_A") -- implicit integer cast
+  ,("aarch32_VQDMLAL_A", "aarch32_VQDMLAL_T1A1_A") -- implicit integer cast
+  ,("aarch32_VQDMLAL_A", "aarch32_VQDMLAL_T2A2_A") -- can't monomorphize Elem
+  ,("aarch32_SSUB8_A", "aarch32_SSUB8_A1_A") -- can't slice lval for setters
+  ,("aarch32_SSUB8_A", "aarch32_SSUB8_T1_A") -- can't slice lval for setters
+  ,("aarch32_SASX_A", "aarch32_SASX_A1_A") -- can't slice lval for setters
+  ,("aarch32_SASX_A", "aarch32_SASX_T1_A") -- can't slice lval for setters
+  ,("aarch32_STRHT_A", "aarch32_STRHT_A1_A") --  encoding is missing "m" field
+  ,("aarch32_STRHT_A", "aarch32_STRHT_A2_A") --  encoding is missing "imm32" field
+  ,("aarch32_STRHT_A", "aarch32_STRHT_T1_A") --  encoding is missing "m" field
+  ,("aarch32_VRINTA_vfp_A", "aarch32_VRINTA_vfp_A1_A") -- TypeNotFound "real"
+  ,("aarch32_VRINTA_vfp_A", "aarch32_VRINTA_vfp_T1_A") -- TypeNotFound "real"
+  ,("aarch32_CPS_AS", "aarch32_CPS_T1_AS") -- encoding is missing "mode" field
+  ,("aarch32_VMAXNM_A", "aarch32_VMAXNM_A1_A") -- TypeNotFound "real"
+  ,("aarch32_VMAXNM_A", "aarch32_VMAXNM_A2_A") -- TypeNotFound "real"
+  ,("aarch32_VMAXNM_A", "aarch32_VMAXNM_T1_A") -- TypeNotFound "real"
+  ,("aarch32_VSTM_A", "aarch32_VSTM_T1A1_A") -- division not supported
+  ,("aarch32_VMAXNM_A", "aarch32_VMAXNM_T2_A") -- TypeNotFound "real"
+  ,("aarch32_VPMAX_f_A", "aarch32_VPMAX_f_A1_A") -- TypeNotFound "real"
   ] 
   
 
@@ -65,7 +89,7 @@ collectInstructions aslInsts = do
 
 ignoredDefs :: [T.Text]
 ignoredDefs =
-  ["AArch64_BranchAddr_1",
+  ["AArch64_BranchAddr_1", -- bad slice
    "AArch32_CheckBreakpoint_2", -- stalls
    "AArch32_VCRMatch_1", -- bad slice
    "AArch32_CheckWatchpoint_4", -- stalls
@@ -87,6 +111,18 @@ ignoredDefs =
    "AArch32_IsExclusiveVA_3", -- simulation abort
    "IsExclusiveGlobal_3", -- simulation abort
    "IsExclusiveLocal_3" -- simulation abort
+  ,"AArch32_WatchpointMatch_5" -- global array
+  ,"AArch32_CheckAdvSIMDOrFPEnabled_2" -- bitmask pattern unimplemented
+  ,"GETTER_D_1" -- division unimplemented, slicing arithmetic
+  ,"SETTER_D_2" -- division unimplemented, slicing arithmetic
+  ]
+
+stalledDefs :: [T.Text]
+stalledDefs =
+  ["AArch32_CheckBreakpoint_2"
+  ,"AArch32_CheckWatchpoint_4"
+  ,"AArch64_CheckBreakpoint_2"
+  ,"AArch64_CheckWatchpoint_4"
   ]
 
 testInstrs :: [(T.Text, T.Text)]
@@ -97,17 +133,40 @@ testInstrs =
 
 main :: IO ()
 main = do
-  _ <- runWithFilters $ Filters (\_ -> True) (\_ -> True) (\_ -> True) (\_ -> True)
+  _ <- runWithFilters filterStalls 0
   return ()
 
-testDefinition :: String -> IO ()
-testDefinition s = do
+testDefinition :: String -> Int -> IO ()
+testDefinition s startidx = do
   let nm = T.pack s
-  sig <- runWithFilters $ translateOnly $ nm
+  sig <- runWithFilters (translateOnlyFun nm) startidx
   case Map.lookup nm (sMap sig) of
     Just _ -> return ()
     Nothing -> error $ "Translation failed to discover function: " <> show nm
+
+testInstruction :: String -> String -> IO ()
+testInstruction instr enc = do
+  _ <- runWithFilters (translateOnlyInstr (T.pack instr, T.pack enc)) 0
+  return ()
+
+translateAll :: Int -> IO ()
+translateAll startidx = do
+  _ <- runWithFilters filterOnlyTranslate startidx
+  return ()
+
+filterOnlyTranslate :: Filters
+filterOnlyTranslate = Filters
+  (\_ -> True)
+  (\_ -> True)
+  (\_ -> False)
+  (\_ -> False)
     
+filterStalls :: Filters
+filterStalls = Filters
+  (\nm -> not $ List.elem nm stalledDefs)
+  (\_ -> True)
+  (\_ -> True)
+  (\_ -> True)
 
 filterIgnored :: Filters
 filterIgnored = Filters
@@ -115,20 +174,37 @@ filterIgnored = Filters
   (\nm -> not $ List.elem nm ignoredInstrs)
   (\_ -> True) (\_ -> True)
 
-translateOnly :: T.Text -> Filters
-translateOnly fnm = Filters
+translateOnlyFun :: T.Text -> Filters
+translateOnlyFun fnm = Filters
   (\nm -> (nm == fnm) || (not $ List.elem nm ignoredDefs))
   (\nm -> not $ List.elem nm ignoredInstrs)
-  (\nm -> nm == fnm) (\_ -> True)
+  (\nm -> nm == fnm) (\_ -> False)
 
-runWithFilters :: Filters -> IO (SigMap)
-runWithFilters filters = do
+translateOnlyInstr :: (T.Text, T.Text) -> Filters
+translateOnlyInstr fnm = Filters
+  (\nm -> False)
+  (\nm -> (nm == fnm))
+  (\nm -> False) (\nm -> nm == fnm)
+
+runWithFilters :: Filters -> Int -> IO (SigMap)
+runWithFilters filters startidx = do
   (aslInsts, aslDefs) <- getASL
   putStrLn $ "Loaded " ++ show (length aslInsts) ++ " instructions and " ++ show (length aslDefs) ++ " definitions."
-  let instrs = collectInstructions aslInsts  
-  MSS.execStateT (forM_ instrs (\(instr, enc) -> runTranslation instr enc aslInsts aslDefs))
-    (SigMap Map.empty filters)
+  let instrs = drop startidx $ imap (\i -> \nm -> (i,nm)) $ collectInstructions aslInsts  
+  MSS.execStateT (forM_ instrs (\(i, (instr, enc)) -> do
+                                  MSS.lift $ putStrLn $ "Processing instruction: " ++ show i ++
+                                                         "/" ++ show (length instrs)
+                                  runTranslation instr enc aslInsts aslDefs))
+    (SigMap Map.empty Map.empty filters)
    
+
+data TranslatorException =
+    TExcept TranslationException
+  | SExcept SigException
+  | SomeExcept X.SomeException
+  
+deriving instance Show TranslatorException
+instance X.Exception TranslatorException
 
 runTranslation :: T.Text -> T.Text -> [AS.Instruction] -> [AS.Definition] -> MSS.StateT SigMap IO ()
 runTranslation instr enc aslInsts aslDefs = do
@@ -140,7 +216,7 @@ runTranslation instr enc aslInsts aslDefs = do
     case computeInstructionSignature instr enc aslInsts aslDefs of
       Left err -> do
         MSS.lift $ putStrLn $ "Error computing instruction signature: " ++ show err
-        MSS.lift $ exitFailure
+        collectError (KeyInstr instr enc) (SExcept err)
       Right (Some (SomeProcedureSignature iSig), instStmts, sigMap) -> do
         MSS.lift $ putStrLn $ "Instruction signature:"
         MSS.lift $ print iSig
@@ -150,7 +226,7 @@ runTranslation instr enc aslInsts aslDefs = do
         case computeDefinitions (Map.keys sigMap) aslDefs of
           Left err -> do
             MSS.lift $ putStrLn $ "Error computing ASL definitions: " ++ show err
-            MSS.lift $ exitFailure
+            collectError (KeyInstr instr enc) (SExcept err) -- FIXME: find function name here
           Right defs -> do
             MSS.lift $ putStrLn $ "Translating instruction: " ++ T.unpack instr ++ " " ++ T.unpack enc
             MSS.lift $ putStrLn $ (show iSig)
@@ -167,9 +243,29 @@ data Filters = Filters { funFilter :: T.Text -> Bool
                        , instrTranslationFilter :: (T.Text, T.Text) -> Bool
                        }
 
+data ElemKey =
+   KeyInstr T.Text T.Text
+ | KeyFun T.Text
+ deriving (Eq, Ord)
+
+
 data SigMap = SigMap { sMap :: Map.Map T.Text (Some (SomeSignature))
+                     , errorMap :: Map.Map ElemKey TranslatorException
                      , sFilters :: Filters
                      }
+
+collectError :: ElemKey -> TranslatorException -> MSS.StateT SigMap IO ()
+collectError k e = MSS.modify' $ \s -> s { errorMap = Map.insert k e (errorMap s) }
+
+catchIO :: ElemKey -> IO a -> MSS.StateT SigMap IO (Maybe a)
+catchIO k f = do
+  a <- MSS.lift ((Left <$> f)
+                  `X.catch` (\(e :: TranslationException) -> return $ Right (TExcept e))
+                  `X.catch` (\(e :: X.SomeException) -> return $ Right (SomeExcept e)))
+  case a of
+    Left r -> return (Just r)
+    Right err -> (\_ -> Nothing) <$> collectError k err
+  
 
 translationLoop :: Definitions arch -> (T.Text, TypeEnvir) -> MSS.StateT SigMap IO ()
 translationLoop defs (fnname, env) = do
@@ -222,53 +318,65 @@ processInstruction :: T.Text
                    -> MSS.StateT SigMap IO (Map.Map T.Text TypeEnvir)
 processInstruction instr enc pSig stmts defs = do
   handleAllocator <- CFH.newHandleAllocator
-  p <- MSS.lift $ procedureToCrucible defs pSig handleAllocator stmts
-  test <- MSS.gets (instrTranslationFilter . sFilters)
-  if not $ test (instr, enc)
-  then do
-    MSS.lift $ putStrLn $ "SKIPPING translation for instruction: " ++ show instr ++ " " ++ show enc
-    return (procDepends p)
-  else do
-    MSS.lift $ CBO.withYicesOnlineBackend globalNonceGenerator CBO.NoUnsatFeatures $ \backend -> do
-      let cfg = SimulatorConfig { simOutputHandle = IO.stdout
-                              , simHandleAllocator = handleAllocator
-                              , simSym = backend
-                              }
-      symFn <- simulateProcedure cfg p
-      return (procDepends p)
-
-processFunction :: T.Text -> SomeSignature ret -> [AS.Stmt] -> Definitions arch -> MSS.StateT SigMap IO (Map.Map T.Text TypeEnvir)
-processFunction fnName sig stmts defs =
-  case sig of
-    SomeFunctionSignature fSig -> do
-      handleAllocator <- CFH.newHandleAllocator
-      f <- MSS.lift $ functionToCrucible defs fSig handleAllocator stmts
-      test <- MSS.gets (funTranslationFilter . sFilters)
-      if not $ test fnName
+  mp <- catchIO (KeyInstr instr enc) $ procedureToCrucible defs pSig handleAllocator stmts
+  case mp of
+    Just p -> do
+      test <- MSS.gets (instrTranslationFilter . sFilters)
+      if not $ test (instr, enc)
       then do
-        MSS.lift $ putStrLn $ "SKIPPING translation for definition: " <> show fnName
-        return (funcDepends f)
-      else do
-        MSS.lift $ CBO.withYicesOnlineBackend globalNonceGenerator CBO.NoUnsatFeatures $ \backend -> do
-          let cfg = SimulatorConfig { simOutputHandle = IO.stdout
-                                  , simHandleAllocator = handleAllocator
-                                  , simSym = backend
-                                  }
-          symFn <- simulateFunction cfg f
-          return (funcDepends f)
-    SomeProcedureSignature pSig -> do
-      handleAllocator <- CFH.newHandleAllocator
-      p <- MSS.lift $ procedureToCrucible defs pSig handleAllocator stmts
-      test <- MSS.gets (funTranslationFilter . sFilters)
-      if not $ test fnName
-      then do
-        MSS.lift $ putStrLn $ "SKIPPING translation for definition: " <> show fnName
+        MSS.lift $ putStrLn $ "SKIPPING translation for instruction: " ++ show instr ++ " " ++ show enc
         return (procDepends p)
       else do
-        MSS.lift $ CBO.withYicesOnlineBackend globalNonceGenerator CBO.NoUnsatFeatures $ \backend -> do
+        mdep <- catchIO (KeyInstr instr enc) $ CBO.withYicesOnlineBackend globalNonceGenerator CBO.NoUnsatFeatures $ \backend -> do
           let cfg = SimulatorConfig { simOutputHandle = IO.stdout
                                   , simHandleAllocator = handleAllocator
                                   , simSym = backend
                                   }
           symFn <- simulateProcedure cfg p
           return (procDepends p)
+        return $ fromMaybe Map.empty mdep
+    Nothing -> return Map.empty
+
+processFunction :: T.Text -> SomeSignature ret -> [AS.Stmt] -> Definitions arch -> MSS.StateT SigMap IO (Map.Map T.Text TypeEnvir)
+processFunction fnName sig stmts defs =
+  case sig of
+    SomeFunctionSignature fSig -> do
+      handleAllocator <- CFH.newHandleAllocator
+      mf <- catchIO (KeyFun fnName) $ functionToCrucible defs fSig handleAllocator stmts
+      case mf of
+        Just f -> do
+          test <- MSS.gets (funTranslationFilter . sFilters)
+          if not $ test fnName
+          then do
+            MSS.lift $ putStrLn $ "SKIPPING translation for definition: " <> show fnName
+            return (funcDepends f)
+          else do
+            mdep <- catchIO (KeyFun fnName) $ CBO.withYicesOnlineBackend globalNonceGenerator CBO.NoUnsatFeatures $ \backend -> do
+              let cfg = SimulatorConfig { simOutputHandle = IO.stdout
+                                      , simHandleAllocator = handleAllocator
+                                      , simSym = backend
+                                      }
+              symFn <- simulateFunction cfg f
+              return (funcDepends f)
+            return $ fromMaybe Map.empty mdep
+        Nothing -> return Map.empty
+    SomeProcedureSignature pSig -> do
+      handleAllocator <- CFH.newHandleAllocator
+      mp <- catchIO (KeyFun fnName) $ procedureToCrucible defs pSig handleAllocator stmts
+      case mp of
+        Just p -> do
+          test <- MSS.gets (funTranslationFilter . sFilters)
+          if not $ test fnName
+          then do
+            MSS.lift $ putStrLn $ "SKIPPING translation for definition: " <> show fnName
+            return (procDepends p)
+          else do
+            mdep <- catchIO (KeyFun fnName) $ CBO.withYicesOnlineBackend globalNonceGenerator CBO.NoUnsatFeatures $ \backend -> do
+              let cfg = SimulatorConfig { simOutputHandle = IO.stdout
+                                      , simHandleAllocator = handleAllocator
+                                      , simSym = backend
+                                      }
+              symFn <- simulateProcedure cfg p
+              return (procDepends p)
+            return $ fromMaybe Map.empty mdep
+        Nothing -> return Map.empty
