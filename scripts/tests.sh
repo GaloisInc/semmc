c32b9a68c831e7562685d8e00b3d9584677b20cd
Daniel Matichuk <dmatichuk@galois.com>
2020-03-10 17:48:50 -0700

    update testing script for asl-lite

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-./submodules/dismantle/scripts/minify-asl.sh
-cd ./submodules/asl-translator/
-make genarm
-cd ../../
-
 cabal v2-build semmc-asl
 cabal v2-test semmc-asl-test-formulas
